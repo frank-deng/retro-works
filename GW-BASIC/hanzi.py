862ceb118986d58f65ee2b09d634f6d1e94626ed
frank-deng <13382084679@163.com>
2017-06-04 16:15:15 +0800

    HANZI.BAS hanzi.py updated

--- GW-BASIC/hanzi.py
+++ GW-BASIC/hanzi.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python3
 
-import sys;
+import sys, struct;
 
 if (len(sys.argv) < 2):
-    sys.stderr.write('Usage: %s X,Y characters\n'%sys.argv[0]);
+    sys.stderr.write('Usage: %s chartable X,Y characters\n'%sys.argv[0]);
     exit(1);
 
-convtab = [
-    0x0, 0x3, 0xC, 0xF,
-    0x30, 0x33, 0x3C, 0x3F,
-    0xC0, 0xC3, 0xCC, 0xCF,
-    0xF0, 0xF3, 0xFC, 0xFF,
-]
-byte2graph = lambda b:convtab[b&0x0F] << 8 | convtab[(b>>4)&0x0F];
-
-count = 0;
-x, y = (sys.argv[1].split(','));
-x, y = int(x), int(y);
-for char in sys.argv[2]:
-    gbcode = char.encode('GB2312', 'ignore');
-    quwei = (gbcode[0]-0xA1, gbcode[1]-0xA1);
-    gdata = [];
-    with open('HZK16', 'rb') as hzk16:
+with open(sys.argv[1], 'rb') as hzk16:
+    x, y = (sys.argv[2].split(','));
+    x, y = int(x), int(y);
+    count = 0;
+    for char in sys.argv[3]:
+        gbcode = char.encode('GB2312', 'ignore');
+        quwei = (gbcode[0]-0xA1, gbcode[1]-0xA1);
+        gdata = [];
         hzk16.seek((quwei[0]*94+quwei[1])*32);
-        for byte in hzk16.read(32):
-            if (byte):
-                gdata.append('&H%x'%byte);
+        for n in struct.unpack('H'*16, hzk16.read(32)):
+            if (n):
+                gdata.append('&H%x'%n);
             else:
                 gdata.append('0');
-    sys.stdout.write('%d DATA %d,%d,%s\r\n'%(100+count, x+count*16, y, ','.join(gdata)));
-    count += 1;
+        sys.stdout.write('%d DATA %d,%d,%s\r\n'%(100+count*10, x+count*16, y, ','.join(gdata)));
+        count += 1;
 
