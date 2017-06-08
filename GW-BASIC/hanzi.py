b8cddfdf7346b8f07860175a1c1445ce2312f2be
frank-deng <13382084679@163.com>
2017-06-08 21:39:30 +0800

    hanzi.py updated

--- GW-BASIC/hanzi.py
+++ GW-BASIC/hanzi.py
@@ -4,14 +4,15 @@ import sys, struct, argparse;
 
 class TextDispParser(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-    	text_all = getattr(namespace, self.dest);
-        try:
-            pos,text = values.split(':');
-            x,y = pos.split(',');
-            x,y = int(x),int(y);
-        except Exception as e:
-            parser.error('Invalid position format.');
-        text_all.append({'pos':(x,y), 'text':text});
+        text_all = getattr(namespace, self.dest);
+        for v in values:
+            try:
+                pos,text = v.split(':');
+                x,y = pos.split(',');
+                x,y = int(x),int(y);
+            except Exception as e:
+                print(str(e));
+            text_all.append({'pos':(x,y), 'text':text});
         setattr(namespace, self.dest, text_all);
 
 argParser = argparse.ArgumentParser();
@@ -23,31 +24,33 @@ argParser.add_argument(
 );
 argParser.add_argument(
     '-o',
+    metavar='output',
     help='Output file.'
 );
 argParser.add_argument(
     'textdisp',
+    metavar='X,Y:Text',
+    nargs='+',
     action=TextDispParser,
     help='Chinese text to display, in the format of X,Y:TEXT.',
     default=[]
 );
 
 class TextDataMaker:
-	def __init__(self, font, output = sys.stdout):
-		self.__font = open(font, 'rb');
-		self.__lineSpace = 10;
-		self.__line = 100;
-		self.__out = output;
-	
-	def __enter__(self):
+    def __init__(self, font, output = sys.stdout):
+        self.__font = open(font, 'rb');
+        self.__lineSpace = 10;
+        self.__line = 100;
+        self.__out = output;
+    
+    def __enter__(self):
         return self;
-	
-	def __exit__(self, exc_type, exc_value, traceback):
-		self.__font.close();
-		
-	def output(self, x, y, text):
-		count = 0;
-		for char in text:
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.__font.close();
+        
+    def output(self, x, y, text):
+        for char in text:
             gbcode = char.encode('GB2312', 'ignore');
             if (len(gbcode) != 2):
                 continue;
@@ -59,18 +62,19 @@ class TextDataMaker:
                     gdata.append('&H%x'%n);
                 else:
                     gdata.append('0');
-            self.__out.write('%d DATA %d,%d,%s\r\n'%(self.__line, x+count*16, y, ','.join(gdata)));
+            self.__out.write('%d DATA %d,%d,%s\r\n'%(self.__line, x, y, ','.join(gdata)));
             self.__line += self.__lineSpace;
-            count += 1;
+            x += 16;
         
 if __name__ == '__main__':
-	args = argParser.parse_args();
-	out = sys.stdout;
-	if args.o:
-	    out = open(args.o, 'w');
-	textDataMaker = TextDataMaker(args.font);
-	for item in args.textdisp:
-	    textDataMaker.output(item['pos'][0], item['pos'][1], item['text']):
-	if args.o:
-	    out.close();
-	exit(0);
+    args = argParser.parse_args();
+    out = sys.stdout;
+    if None != args.o:
+        out = open(args.o, 'w');
+    textDataMaker = TextDataMaker(args.font, out);
+    for item in args.textdisp:
+        textDataMaker.output(item['pos'][0], item['pos'][1], item['text']);
+    if out != sys.stdout:
+        out.close();
+    exit(0);
+
