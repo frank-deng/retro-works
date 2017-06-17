f9cefd837bb824418d7598e9222fd6a1a0a75ffe
frank-deng <13382084679@163.com>
2017-06-18 00:28:03 +0800

    Updated ie3-site

---
+++ ie3-site/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+jekyll b
+
+cd _site
+for fname in $(ls *.html); do
+	fname=$(basename ${fname});
+	iconv -ct gbk "${fname}" > "${fname%.*}.htm"
+done;
+rm *.html &>/dev/null
+cd ..
+
+SITEDIR=$(date +%y%m%d)
+mkdir $SITEDIR
+cp -aR _site/* $SITEDIR
+rm site.zip &>/dev/null
+zip -r site $SITEDIR/*
+rm -r $SITEDIR
+
