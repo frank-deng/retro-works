31a05286c5b014353c3d65b00ef3f787c3c59c77
frank-deng <13382084679@163.com>
2017-07-29 12:05:49 +0800

    mkphotofloppy.sh

---
+++ mkphotofloppy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+SOURCE="${1}"
+TARGET="${2}"
+quality=50
+counter=1
+imgcounter=1
+
+TEMPFILE=$(tempfile);
+IMGFILE='';
+
+mknewdisk() {
+	IMGFILE="${TARGET}/"photo$(printf %03d $imgcounter).img;
+	dd if=/dev/zero of="${IMGFILE}" bs=512 count=2880 &>/dev/null
+	mformat -i "${IMGFILE}" -f 1440
+	imgcounter=$((imgcounter+1));
+}
+
+rm "${TARGET}"/*.img &>/dev/null
+mknewdisk
+find "${SOURCE}" -type f -iname \*.jpg | sort | while read file; do
+	newfile=DSC$(printf %05d $counter).jpg;
+	convert "${file}" -resize '640x640>' -quality $quality "${TEMPFILE}"
+	mcopy -i "${IMGFILE}" "${TEMPFILE}" ::/"${newfile}" &>/dev/null
+	if [[ $? -ne 0 ]]; then
+		mknewdisk
+		mcopy -i "${IMGFILE}" "${TEMPFILE}" ::/"${newfile}" &>/dev/null
+	fi;
+	counter=$((counter+1));
+done;
+rm "${TEMPFILE}"
+
