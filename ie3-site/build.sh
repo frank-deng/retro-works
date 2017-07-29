1f10e38519a83e2426af50b2889437e69a4a9a2f
frank-deng <13382084679@163.com>
2017-07-29 11:31:04 +0800

    ie3-site build.sh updated

--- ie3-site/build.sh
+++ ie3-site/build.sh
@@ -9,4 +9,12 @@ done;
 rm *.html &>/dev/null
 find . -iname \*.jpg -exec mogrify -resize '600x300>' {} \;
 cd ..
-genisoimage -o mysite.iso -V MYSITE _site/
+
+TARGET='myspace.img';
+rm "${TARGET}"
+dd if=/dev/zero of="${TARGET}" bs=512 count=2880
+mformat -i "${TARGET}" -f 1440
+mlabel -i "${TARGET}" ::MYSPACE
+cd _site
+mcopy -i "../${TARGET}" -s * ::/
+cd ..
