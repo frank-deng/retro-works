985276c7d20705475191dd93c434d1b2ae7a67b4
frank-deng <13382084679@163.com>
2017-11-08 17:39:39 +0800

    Updated

--- 24solve.php
+++ 24solve.php
@@ -115,36 +115,39 @@ $exprAll = [
 	[expr=>"(%s-%s/%s)*%s", sort=>null],
 ];
 ?>DEFINT E-Z:DEFDBL A-D,R
-COMMON SHARED A,B,C,D,R,GOAL,CANS
-CANS=0
+COMMON SHARED A,B,C,D,R,GOAL,FOUND
 
 SUB CALC
 <?php foreach ($exprAll as $e){?>
-    R=<?=sprintf($e[expr],'A','B','C','D')?>:IF R=GOAL THEN PRINT <?=str_replace('"";','',sprintf('"'.$e[expr].'=";R','";A;"','";B;"','";C;"','";D;"'))?>:GOTO FOUND
+    R=<?=sprintf($e[expr],'A','B','C','D')?>:IF R=GOAL THEN PRINT <?=str_replace('"";','',sprintf('"'.$e[expr].'=";R','";A;"','";B;"','";C;"','";D;"'))?>:GOTO LFOUND
 <?php } ?>
-    GOTO CALCFINISH
-    FOUND:
-    CANS=1
-    CALCFINISH:
+    EXIT SUB
+LFOUND:
+    FOUND=1 : EXIT SUB
 END SUB
 
 SUB MAIN
-    DIM N(4)
+    DIM N(4) : FOUND=0
     INPUT "A,B,C,D,Goal:",N(0),N(1),N(2),N(3),GOAL
     FOR I=0 TO 3
-        FOR J=0 TO 3
-           IF I=J THEN GOTO SKIPJ
-           FOR K=0 TO 3
-               IF I<>K AND J<>K THEN
-                   A=N(I):B=N(J):C=N(K):D=N(6-I-J-K)
-                   CALC
-                   IF CANS > 0 THEN GOTO FINISH
-               END IF
-           NEXT K
-SKIPJ:
-        NEXT J
+    FOR J=0 TO 3
+      IF I<>J THEN
+      FOR K=0 TO 3
+        IF I<>K AND J<>K THEN
+          A=N(I):B=N(J):C=N(K):D=N(6-I-J-K)
+          CALC
+          IF FOUND > 0 THEN EXIT SUB
+        END IF
+      NEXT K
+      END IF
+    NEXT J
     NEXT I
     PRINT "No Answer."
-FINISH:
 END SUB
+
+ON ERROR GOTO DOERROR
 MAIN
+GOTO FINISH
+DOERROR:
+RESUME NEXT
+FINISH:
