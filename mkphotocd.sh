5f54cb2bcd7a0c6b45a5674eecad9fc82eabdda6
frank-deng <13382084679@163.com>
2018-04-07 23:02:37 +0800

    Updated

--- mkphotocd.sh
+++ mkphotocd.sh
@@ -5,7 +5,7 @@ quality=80
 counter=1
 find "${SOURCE}" -type f -iname \*.jpg | sort | while read file; do
 	newfile=DSC$(printf %05d $counter).jpg;
-	convert "${file}" -resize '640x480>' -quality $quality "${TARGET}/${newfile}"
+	convert "${file}" -strip -resize '640x480>' -quality $quality "JPEG:${TARGET}/${newfile}"
 	counter=$((counter+1));
 done;
 
