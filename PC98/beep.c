25d44dee274150bbe0642f77d8734815d2bb4528
frank-deng <13382084679@163.com>
2018-07-07 14:22:28 +0800

    Updated beep.c

--- PC98/beep.c
+++ PC98/beep.c
@@ -4,26 +4,6 @@
 #include <farstr.h>
 #include <time.h>
 
-#define	PICPORT		0x00		/* interrupt controller (int 08h = master) */
-#define	EOI			0x20		/* end of interrupt */
-#define	IMR_M		0x02		/* IMR r/w port(master) */
-#define	TMRIMRBIT	0x01		/* timer IRQ mask bit */
-#define	RS2IMRBIT	0x10		/* rs-232c IRQ mask bit */
-#define	IMR_S		0x0a		/* IMR r/w port(slave) */
-#define	MOSIMRBIT	0x20		/* mouse IRQ mask bit */
-#define	TMRINTVEC	0x08		/* timer interrupt vector number */
-
-#define	TMRMODE		0x77		/* timer mode port */
-#define	TMR0MOD3	0x36		/* timer #0, mode 3(square) */
-#define	TMR1MOD0	0x70		/* timer #1, mode 0(interval timer mode) */
-#define	TMR1MOD3	0x76		/* timer #1, mode 3(square wave generator mode) */
-
-#define	TMR0CLK		0x71		/* timer #0 counter set port */
-#define	TMR1CLK		0x3fdb		/* timer #1 counter set port */
-
-#define	SYSPORTC	0x35		/* system port C */
-#define	BUZ_BIT		0x08		/* buzzer bit  0:on, 1:off */
-
 typedef enum _action_t{
 	ACTION_QUIT,
 }action_t;
@@ -39,8 +19,9 @@ action_t getaction(){
 	}
 }
 
-static unsigned int	count;
 static void setfreq(unsigned int timer, unsigned int freq){
+#define	TMR0CLK		0x71		/* timer #0 counter set port */
+#define	TMR1CLK		0x3fdb		/* timer #1 counter set port */
 #define	BIOS_FLAG	*(unsigned char far *)0x00000501L	/* BIOS_FLAG at system area */
 #define	SYSCLK_BIT	0x80		/* system clock bit  0:5/10/20MHz, 1:8/16MHz */
 #define	COUNT5		2457600L	/* timer clock freq.[Hz] at 5/10/20MHz */
@@ -52,22 +33,55 @@ static void setfreq(unsigned int timer, unsigned int freq){
 	else {									/* 5MHz/10MHz... */
 		count = (unsigned int)((double)COUNT5/(double)freq+0.5);
 	}
-	outp(timer, (int)(count&0x00ff));		/* rate LSB */
-	outp(timer, (int)(count>>8));			/* rate MSB */
+	if (timer) {
+		outp(TMR1CLK, (int)(count&0x00ff));		/* rate LSB */
+		outp(TMR1CLK, (int)(count>>8));			/* rate MSB */
+	} else {
+		outp(TMR0CLK, (int)(count&0x00ff));		/* rate LSB */
+		outp(TMR0CLK, (int)(count>>8));			/* rate MSB */
+	}
 }
+
+void (far *OrgTimerVect)();
+unsigned int num = 0;
+
+static void NewTimerVect(void) {
+#define	PICPORT		0x00		/* interrupt controller (int 08h = master) */
+#define	EOI			0x20		/* end of interrupt */
+	num = 0;
+	outp(PICPORT, EOI);
+}
+
+#define	IMR_M		0x02		/* IMR r/w port(master) */
+#define	TMRIMRBIT	0x01		/* timer IRQ mask bit */
+#define	IMR_S		0x0a		/* IMR r/w port(slave) */
+#define	TMRINTVEC	0x08		/* timer interrupt vector number */
+
+#define	TMRMODE		0x77		/* timer mode port */
+#define	TMR0MOD0	0x30		/* timer #0, mode 0(interval timer mode) */
+#define	TMR0MOD3	0x36		/* timer #0, mode 3(square) */
+#define	TMR1MOD0	0x70		/* timer #1, mode 0(interval timer mode) */
+#define	TMR1MOD3	0x76		/* timer #1, mode 3(square wave generator mode) */
+
+#define	SYSPORTC	0x35		/* system port C */
+#define	BUZ_BIT		0x08		/* buzzer bit  0:on, 1:off */
 int main(){
-	setfreq(TMR1CLK, 1000);
+	setfreq(1, 2000);
 
 	outp(TMRMODE, TMR1MOD3);				/* counter mode 3 */
 	outp(SYSPORTC, (inp(SYSPORTC)&(~BUZ_BIT)));	/* beep on */
 
-	while (ACTION_QUIT != getaction()){
+	while (!kbhit()){
 	}
-	setfreq(TMR1CLK, 2000);
-	while (ACTION_QUIT != getaction()){
-	}
-	outp(SYSPORTC, (inp(SYSPORTC)|BUZ_BIT));		/* beep off */
+	getch();
 
+	setfreq(1, 1000);
+
+	while (!kbhit()){
+	}
+	getch();
+
+	outp(SYSPORTC, (inp(SYSPORTC)|BUZ_BIT));		/* beep off */
 	return 0;
 }
 
