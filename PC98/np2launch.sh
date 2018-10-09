a7e42f9d8c8a2c6478263be36e235efe751b4273
frank-deng <13382084679@163.com>
2018-10-09 22:59:01 +0800

    Updated

--- PC98/np2launch.sh
+++ PC98/np2launch.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 xrandr --output LVDS-1-1 --mode 1024x576
-feh -F black.png &>/dev/null &
-PID=$!
+PID="";
+if [[ -f /usr/local/share/icons/black.png ]]; then
+	feh -F /usr/local/share/icons/black.png &>/dev/null &
+	PID=$!
+else
+	feh -F black.png &>/dev/null &
+	PID=$!
+fi
 xnp2kai &>/dev/null &
 PID_NP2=$!
-wmctrl -r Neko -e 0,192,87,-1,-1
-wait $PID_NP2
-kill -2 $PID
+sleep 0.5
+wmctrl -a Neko
+wmctrl -r Neko -e 0,192,0,-1,-1
+wait $PID_NP2;
+
 xrandr --output LVDS-1-1 --mode 1366x768
+kill -2 $PID
 
