50b395d8c3864d51baca7e40dc37ae727bca73b8
frank-deng <13382084679@163.com>
2019-10-03 11:35:59 +0800

    Photo CD generate larger photo

--- misc/mkphotocd.sh
+++ misc/mkphotocd.sh
@@ -6,8 +6,9 @@ quality=80
 counter=1
 find "${SOURCE}" -maxdepth 1 -type f -iname \*.jpg | sort | while read file; do
 	newfile=DSC$(printf %05d $counter).jpg;
-	convert "${file}" -strip -resize '640x640>' -quality $quality "JPEG:${TEMPDIR}/${newfile}"
+	convert "${file}" -strip -resize '1280x1280>' -quality $quality "JPEG:${TEMPDIR}/${newfile}"
 	counter=$((counter+1));
 done;
 genisoimage -o "${TARGET}" "${TEMPDIR}"
 rm -r "${TEMPDIR}"
+
