e0dd800dd18e6bb7d05b853cbee9ae5e56da2a84
frank-deng <13382084679@163.com>
2021-01-02 00:55:02 +0800

    Telnet server updated

--- PC98/np2redirect.js
+++ PC98/np2redirect.js
@@ -53,6 +53,7 @@ async function readln(stream,param={}){
         return result;
     }).catch((e)=>{
         clearTimeout(timer);
+        throw e;
     });
 }
 class LoginManager{
@@ -65,69 +66,73 @@ class LoginManager{
     destroy(){
     }
     async main(){
-        //等待输入用户名，未输入文本时，3s后重来，输入文本后30s内不按回车则重来
-        let username=null;
-        while(!username){
-            this.stream.write('Login:');
-            username=await readln(this.stream,{
-                emptyTimeout:3,
-                inactiveTimeout:30
-            });
-            if(!username){
-                this.stream.write('                                        \r');
+        try{
+            //等待输入用户名，未输入文本时，3s后重来，输入文本后30s内不按回车则重来
+            let username=null;
+            while(!username){
+                this.stream.write('Login:');
+                username=await readln(this.stream,{
+                    emptyTimeout:3,
+                    inactiveTimeout:30
+                });
+                if(!username){
+                    this.stream.write('                                        \r');
+                }
             }
-        }
 
-        //等待输入密码，30后无反应视为非法登录并要重新输入用户名
-        this.stream.write('Password:');
-        let password=await readln(this.stream,{
-            emptyTimeout:30,
-            inactiveTimeout:30,
-            echo:false
-        });
-        if(!password){
-            this.stream.write('Invalid Login.');
-            this.main();
-            return;
-        }
+            //等待输入密码，30后无反应视为非法登录并要重新输入用户名
+            this.stream.write('Password:');
+            let password=await readln(this.stream,{
+                emptyTimeout:30,
+                inactiveTimeout:30,
+                echo:false
+            });
+            if(!password){
+                this.stream.write('Invalid Login.');
+                this.main();
+                return;
+            }
 
-        //根据用户名和密码查找对应的应用
+            //根据用户名和密码查找对应的应用
 
-        //Redirect everything to ppp
-        this.stream.write('Success\r\n');
-        let pppClient=net.connect({
-            host:TARGET_HOST,
-            port:TARGET_PORT
-        },function(){
-            _log('Remote connected');
-        });
-        this.stream.on('data',(data)=>{
-            pppClient.write(data);
-        });
+            //Redirect everything to ppp
+            this.stream.write('Success\r\n');
+            let pppClient=net.connect({
+                host:TARGET_HOST,
+                port:TARGET_PORT
+            },function(){
+                _log('Remote connected');
+            });
+            this.stream.on('data',(data)=>{
+                pppClient.write(data);
+            });
 
-        //根据返回的数据检测当前PPP连接是否需要断开
-        let endStream=[0x7e,0xff,0x7d,0x23,0xc0,0x21,0x7d,0x26];
-        pppClient.on('data',(data)=>{
-            if(data.length > endStream.length){
-                let disconnect=true;
-                for(let i=0; i<endStream.length; i++){
-                    if(data[i]!=endStream[i]){
-                        disconnect=false;
-                        break;
+            //根据返回的数据检测当前PPP连接是否需要断开
+            let endStream=[0x7e,0xff,0x7d,0x23,0xc0,0x21,0x7d,0x26];
+            pppClient.on('data',(data)=>{
+                if(data.length > endStream.length){
+                    let disconnect=true;
+                    for(let i=0; i<endStream.length; i++){
+                        if(data[i]!=endStream[i]){
+                            disconnect=false;
+                            break;
+                        }
+                    }
+                    if(disconnect){
+                        pppClient.destroy();
+                        this.main();
+                        return;
                     }
                 }
-                if(disconnect){
-                    pppClient.destroy();
-                    this.main();
-                    return;
-                }
-            }
-            this.stream.write(data);
-        });
-        pppClient.on('error',()=>{
-            _log('Remote error');
-            this.main();
-        });
+                this.stream.write(data);
+            });
+            pppClient.on('error',()=>{
+                _log('Remote error');
+                this.main();
+            });
+        }catch(e){
+            console.error(e);
+        }
     }
 }
 
@@ -135,6 +140,10 @@ const pipe=net.createServer((stream)=>{
     let loginManager=new LoginManager(stream);
     stream.on('end', function() {
         _log('Guest closed connection');
+        loginManager.destroy();
+    });
+    stream.on('error', function() {
+        _log('Named pipe error');
         loginManager.destroy();
     });
     _log('Guest connected');
