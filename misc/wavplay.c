546e35b3892bf7714163f0a4eb416007e79d2ce8
frank-deng <13382084679@163.com>
2021-06-29 16:25:08 +0800

    wavplay.c updated

--- misc/wavplay.c
+++ misc/wavplay.c
@@ -162,31 +162,40 @@ Timer related code
 static wav_t *wavInstance;
 static uint8_t Port61hValueOrig;
 static void (__interrupt __far * timerHandlerOrig)();
+static uint8_t microSecTable[]={
+    0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,8,9,9,9,9,10,10,
+    10,10,11,11,11,11,12,12,12,12,12,13,13,13,13,14,14,14,14,15,15,15,15,16,16,16,16,16,17,17,
+    17,17,18,18,18,18,19,19,19,19,20,20,20,20,20,21,21,21,21,22,22,22,22,23,23,23,23,24,24,24,24,
+    24,25,25,25,25,26,26,26,26,27,27,27,27,28,28,28,28,28,29,29,29,29,30,30,30,30,31,31,31,31,32,32,
+    32,32,32,33,33,33,33,34,34,34,34,35,35,35,35,36,36,36,36,36,37,37,37,37,38,38,38,38,39,39,39,39,
+    40,40,40,40,40,41,41,41,41,42,42,42,42,43,43,43,43,44,44,44,44,44,45,45,45,45,46,46,46,46,47,47,47,
+    47,48,48,48,48,48,49,49,49,49,50,50,50,50,51,51,51,51,52,52,52,52,52,53,53,53,53,54,54,54,54,55,55,55,
+    55,56,56,56,56,56,57,57,57,57,58,58,58,58,59,59,59,59,60,60,60
+};
 void __interrupt timerHandler(){
     uint16_t value=getSample(wavInstance);
-    uint16_t microsec=(value*60/255);
     outp(0x43,0x96);
-    outp(0x42,microsec&0xff);
+    outp(0x42,microSecTable[value]&0xff);
     outp(0x20,0x20);
 }
 void initTimer(unsigned int reloadValue){
-    timerHandlerOrig=_dos_getvect(0x08);
-    _dos_setvect(0x08,timerHandler);
-    puts("ok");
+    cli();
     if(reloadValue<0xff){
         outp(0x43,0x14);
         outp(0x40,reloadValue&0xff);
     }else{
-        puts("ok1");
         outp(0x43,0x34);
-        printf("%u\n",reloadValue);
         outp(0x40,reloadValue&0xff);
         outp(0x40,(reloadValue>>8)&0xff);
-        puts("ok2");
     }
     Port61hValueOrig=inp(0x61);
     outp(0x61,Port61hValueOrig&0x3);
+    puts("ok");
+    timerHandlerOrig=_dos_getvect(0x08);
+    puts("ok2");
+    _dos_setvect(0x08,timerHandler);
     puts("ok3");
+    sti();
 }
 void closeTimer(){
     outp(0x61,Port61hValueOrig);
