003a0df3bd5e16ac2f47386680e131f253d0de16
frank-deng <13382084679@163.com>
2021-07-31 13:33:17 +0800

    setup script updated

--- telnet-ppp-server/setup.py
+++ telnet-ppp-server/setup.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
-import os
-from setuptools import setup
+import os,sys;
+from setuptools import setup;
+
+if 'linux'!=sys.platform:
+    print("This package only supports Linux platform.");
+    exit(1);
 
 setup(
     name = 'telnet-ppp-server', # 在pip中显示的项目名称
     version = '0.1',
     author = 'Frank',
     author_email = '',
-    licence = 'MIT',
+    license = 'MIT',
     url = '',
     description = 'PPP server and Telnet server for old platforms like Windows 3.x, Windows 95.',
     python_requires = '>=3.5.0',
     py_modules=['serverLib'],
-    scripts=['pppd.py','telnetd.py','raw-mode']
+    entry_points={
+        'console_scripts':[
+            'pppd.py = pppd',
+            'telnetd.py = telnetd'
+        ]
+    },
+    scripts=['raw-mode']
 );
-
