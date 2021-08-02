03968d5d2505f25ec1a1aff034a882d777a0182f
frank-deng <13382084679@163.com>
2021-08-02 20:34:03 +0800

    ncov table add increment disp with color

--- retro-site/ncovtable.php
+++ retro-site/ncovtable.php
@@ -2,6 +2,19 @@
 $data=apcu_fetch('ncov_data');
 if($data){
     $desc=$data['desc'];
+function display_incr($val){
+    $val=intval($val);
+    if(0==$val){
+        return '';
+    }
+    if($val>0){
+        $val='+'.$val;
+        $color='red';
+    }else{
+        $color='green';
+    }
+    return ' <font color=\''.$color.'\'>('.$val.')</font>';
+}
 ?><table width='100%' cellspacing='0'>
     <tr>
         <th width='33%' align='center'><font face='宋体'>现存确诊人数</font></th>
@@ -9,9 +22,9 @@ if($data){
         <th width='33%' align='center'><font face='宋体'>累计治愈人数</font></th>
     </tr>
     <tr>
-        <td align='center'><font face='Times New Roman'><?=$desc['currentConfirmedCount']?></font></td>
-        <td align='center'><font face='Times New Roman'><?=$desc['confirmedCount']?></font></td>
-        <td align='center'><font face='Times New Roman'><?=$desc['curedCount']?></font></td>
+        <td align='center'><font face='Times New Roman'><?=$desc['currentConfirmedCount']?><?=display_incr($desc['currentConfirmedIncr'])?></font></td>
+        <td align='center'><font face='Times New Roman'><?=$desc['confirmedCount']?><?=display_incr($desc['confirmedIncr'])?></font></td>
+        <td align='center'><font face='Times New Roman'><?=$desc['curedCount']?><?=display_incr($desc['curedIncr'])?></font></td>
     </tr>
     <tr>
         <th align='center'><font face='宋体'>现存无症状人数</font></th>
@@ -19,9 +32,9 @@ if($data){
         <th align='center'><font face='宋体'>累计死亡人数</font></th>
     </tr>
     <tr>
-        <td align='center'><font face='Times New Roman'><?=$desc['seriousCount']?></font></td>
-        <td align='center'><font face='Times New Roman'><?=$desc['suspectedCount']?></font></td>
-        <td align='center'><font face='Times New Roman'><?=$desc['deadCount']?></font></td>
+        <td align='center'><font face='Times New Roman'><?=$desc['seriousCount']?><?=display_incr($desc['seriousIncr'])?></font></td>
+        <td align='center'><font face='Times New Roman'><?=$desc['suspectedCount']?><?=display_incr($desc['suspectedIncr'])?></font></td>
+        <td align='center'><font face='Times New Roman'><?=$desc['deadCount']?><?=display_incr($desc['deadIncr'])?></font></td>
     </tr>
 </table><?php
 }
