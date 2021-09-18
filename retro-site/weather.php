7c5482191783a1445771eb4b11cbfdbbe5caee2b
frank-deng <13382084679@163.com>
2021-09-18 21:29:53 +0800

    Updated weather detail

--- retro-site/weather.php
+++ retro-site/weather.php
@@ -13,74 +13,62 @@ foreach(explode('&',$_SERVER['QUERY_STRING']) as $item){
     $_QUERY[$key]=$value;
 }
 
-$location=iconv('GB2312','UTF-8',rawurldecode($_COOKIE['location']));
+$location=null;
 if($_QUERY['location']){
-		$location=$_QUERY['location'];
-		setcookie("location",$location,time()+3600*24*666);
-}else if(!$location){
-		header('Location: selectCity.php');
-		exit();
+	$location=$_QUERY['location'];
+	setcookie("location",$location,time()+3600*24*666);
+}else if($_COOKIE['location']){
+    $location=$_COOKIE['location'];
+}
+if(!$location){
+    header('Location: selectCity.php');
+    exit();
 }
 
-$location=explode(';',$location);
-$location_id=$location[0];
-$location_name=$location[1];
-
-try{
-	$suggestion_text=array(
-		'air'=>'空气污染指数：',
-		'comf'=>'人体舒适指数：',
-		'cw'=>'洗车指数：',
-		'drsg'=>'穿衣指数：',
-		'flu'=>'感冒指数：',
-		'sport'=>'运动指数：',
-		'trav'=>'旅行指数：',
-		'uv'=>'紫外线指数：'
-	);
-	$loadWeather=new FetchWeather();
+$loadWeather=null;
+if($location){
+    $location=explode(';',$location);
+    $loadWeather=new FetchWeather($location[0],$location[1]);
     fetchMultiWait($loadWeather);
-	$weather = $loadWeather->fetch();
-	if(!$weather){
-		die('Failed to load weather data.');
-	}
-	$_TITLE=$_HEADER='天气预报';
-}catch(Exception $e){
-	error_log($e);
-	die($e->getMessage());
 }
 
+$weather=$loadWeather->fetch();
+
+$_TITLE=$_HEADER='天气预报';
 require('header.php');
-?>
-<p></p>
+if(!$weather){
+    echo '<h3>没有天气信息</h3>';
+    require('footer.php');
+    exit();
+}
+?><p></p>
 <table>
 	<tr>
 		<th align='left'>天气</th>
 		<td rowspan='3' width='10px'></td>
-		<td><?=$weather['now']['cond']['txt']?></td>
+		<td><?=$weather['now']['text']?></td>
 		<td rowspan='3' width='20px'></td>
 		<th align='left'>温度</th>
 		<td rowspan='3' width='10px'></td>
-		<td><?=$weather['daily_forecast'][0]['tmp']['min']?>℃/<?=$weather['daily_forecast'][0]['tmp']['max']?>℃</td>
+		<td><?=$weather['now']['temp']?>℃</td>
 		<td rowspan='3' width='20px'></td>
-		<th align='left'>紫外线指数</th>
+		<th align='left'>空气污染指数</th>
 		<td rowspan='3' width='10px'></td>
-		<td><?=$weather['daily_forecast'][0]['uv']?></td>
+		<td><?=$weather['air']['aqi']?></td>
 	</tr>
 	<tr>
 		<th align='left'>湿度</th>
-		<td><?=$weather['daily_forecast'][0]['hum']?>%</td>
+		<td><?=$weather['now']['humidity']?>%</td>
 		<th align='left'>能见度</th>
-		<td><?=$weather['daily_forecast'][0]['vis']?> km</td>
-		<th align='left'>空气污染指数</th>
-		<td><?=$weather['aqi']['city']['aqi']?></td>
+		<td><?=$weather['now']['vis']?> km</td>
+		<th align='left'>空气质量等级</th>
+		<td><?=$weather['air']['category']?></td>
 	</tr>
 	<tr>
 		<th align='left'>风向</th>
-		<td><?=$weather['daily_forecast'][0]['wind']['dir']?></td>
+		<td><?=$weather['now']['windDir']?></td>
 		<th align='left'>风速</th>
-		<td><?=$weather['daily_forecast'][0]['wind']['sc']?>级</td>
-		<th align='left'>空气质量等级</th>
-		<td><?=$weather['aqi']['city']['qlty']?></td>
+		<td><?=$weather['now']['windScale']?>级</td>
 	</tr>
 </table>
 <div align='center'><img src='/static/ELEGLINE.GIF'/></div>
@@ -96,16 +84,16 @@ require('header.php');
 		<th align='left'>风向</th>
 		<th align='left'>风速</th>
 	</tr>
-<?php foreach ($weather['daily_forecast'] as $idx=>$forecast){ if($idx>0){ ?>
+<?php foreach ($weather['forecast'] as $idx=>$forecast){ if($idx>0){ ?>
 	<tr>
-		<td><?=$forecast['date']?></td>
-		<td><?=$forecast['cond']['txt_d'].'/'.$forecast['cond']['txt_n']?></td>
-		<td><?=$forecast['tmp']['min']?>℃/<?=$forecast['tmp']['max']?>℃</td>
-		<td><?=$forecast['hum']?>%</td>
-		<td><?=$forecast['uv']?></td>
+		<td><?=$forecast['fxDate']?></td>
+		<td><?=$forecast['textDay'].'/'.$forecast['textNight']?></td>
+		<td><?=$forecast['tempMin']?>℃/<?=$forecast['tempMax']?>℃</td>
+		<td><?=$forecast['humidity']?>%</td>
+		<td><?=$forecast['uvIndex']?></td>
 		<td><?=$forecast['vis']?> km</td>
-		<td><?=$forecast['wind']['dir']?></td>
-		<td><?=$forecast['wind']['sc']?>级</td>
+		<td><?=$forecast['windDirDay']?>/<?=$forecast['windDirNight']?></td>
+		<td><?=$forecast['windScaleDay']?>级/<?=$forecast['windScaleNight']?>级</td>
 	</tr>
 <?php }} ?>
 </table>
