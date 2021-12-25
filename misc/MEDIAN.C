3698abff8c2490230abdcb16ab564e3dc213655b
frank-deng <13382084679@163.com>
2021-12-26 01:23:14 +0800

    Updated

---
+++ misc/MEDIAN.C
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef enum{
+	MIN_HEAP,
+	MAX_HEAP
+}heap_type_t;
+typedef struct{
+	heap_type_t type;
+	size_t length;
+	size_t itemSize;
+	size_t size;
+	void* data;
+}heap_t;
+int heap_init(heap_t* heap,	heap_type_t type,	size_t itemSize, size_t size){
+	heap->type=type;
+	heap->length=0;
+	heap->size=size;
+	heap->itemSize=itemSize;
+	heap->data=(void*)malloc(itemSize*size);
+	return heap->data ? 1 : 0;
+}
+void heap_close(heap_t *heap){
+	free(heap->data);
+	heap->length=heap->size=heap->itemSize=0;
+	heap->data=NULL;
+}
+
+typedef int item_t;
+int heap_push(heap_t *heap, item_t value){
+	size_t sizeNew;
+	void* dataNew;
+	size_t pos=heap->length, parentPos;
+	item_t *heapData, temp;
+
+	/* Heap space memory expansion */
+	if(heap->length >= heap->size){
+		sizeNew=heap->size+((heap->size)>>1);
+		dataNew=(void*)realloc(heap->data,(heap->itemSize)*sizeNew);
+		if(!dataNew){
+			return 0;
+		}
+		heap->data=dataNew;
+		heap->size=sizeNew;
+	}
+
+	/* Handle heap push */
+	heapData=(item_t*)(heap->data);
+	heapData[heap->length]=value;
+	heap->length++;
+	while(pos){
+		parentPos=(pos-1)>>1;
+		if((MIN_HEAP==heap->type && heapData[parentPos]<=heapData[pos])
+			|| (MAX_HEAP==heap->type && heapData[parentPos]>=heapData[pos])){
+			break;
+		}
+		temp=heapData[parentPos];
+		heapData[parentPos]=heapData[pos];
+		heapData[pos]=temp;
+		pos=parentPos;
+	}
+	return 1;
+}
+item_t heap_top(heap_t *heap){
+	return ((item_t*)(heap->data))[0];
+}
+item_t heap_pop(heap_t *heap){
+	item_t *arrData=(item_t*)(heap->data), heapTopOrig,
+		value, leftVal, rightVal, temp;
+	size_t pos=0, length, leftPos, rightPos;
+	if(!(heap->length)){
+		return 0;
+	}
+	heapTopOrig=arrData[0];
+	(heap->length)--;
+	if(!(heap->length)){
+		return heapTopOrig;
+	}
+	arrData[0]=arrData[heap->length];
+	length=heap->length;
+	while(pos<length){
+		value=arrData[pos]; leftPos=(pos<<1)+1; rightPos=(pos<<1)+2;
+		if(rightPos>=length){
+			if(leftPos>=length){
+				break;
+			}
+			leftVal=arrData[leftPos];
+			if((MIN_HEAP==heap->type && leftVal<value)
+				||(MAX_HEAP==heap->type && leftVal>value)){
+				temp=arrData[leftPos];
+				arrData[leftPos]=arrData[pos];
+				arrData[pos]=temp;
+			}
+			break;
+		}
+		leftVal=arrData[leftPos]; rightVal=arrData[rightPos];
+		if((MIN_HEAP==heap->type && value<=leftVal && value<=rightVal)
+			||(MAX_HEAP==heap->type && value>=leftVal && value>=rightVal)){
+			break;
+		}
+		if((MIN_HEAP==heap->type && leftVal<=rightVal)
+			||(MAX_HEAP==heap->type && leftVal>=rightVal)){
+			temp=arrData[leftPos];
+			arrData[leftPos]=arrData[pos];
+			arrData[pos]=temp;
+			pos=leftPos;
+		}else{
+			temp=arrData[rightPos];
+			arrData[rightPos]=arrData[pos];
+			arrData[pos]=temp;
+			pos=rightPos;
+		}
+	}
+	return heapTopOrig;
+}
+void print_data(item_t *src, size_t length){
+	size_t i;
+	for(i=0; i<length; i++){
+		printf("%8d",src[i]);
+	}
+	putchar('\n');
+}
+int main(){
+	heap_t heapL, heapR;
+	item_t input, topL, topR;
+	heap_init(&heapL, MAX_HEAP, sizeof(item_t), 128);
+	heap_init(&heapR, MIN_HEAP, sizeof(item_t), 128);
+	while(EOF!=scanf("%d",&input)){
+		if(!heapL.length && !heapR.length){
+			heap_push(&heapL,input);
+			continue;
+		}
+		topL=heap_top(&heapL);
+		if(!heapR.length){
+			if(input>=topL){
+				heap_push(&heapR,input);
+			}else{
+				heap_push(&heapR,heap_pop(&heapL));
+				heap_push(&heapL,input);
+			}
+			continue;
+		}
+		topR=heap_top(&heapR);
+		if(input<=topL){
+			heap_push(&heapL,input);
+			if((heapL.length-heapR.length) > 1){
+				heap_push(&heapR,heap_pop(&heapL));
+			}
+		}else if(input>=topR){
+			heap_push(&heapR,input);
+			if(heapR.length!=heapL.length){
+				heap_push(&heapL,heap_pop(&heapR));
+			}
+		}else if(heapL.length != heapR.length){
+			heap_push(&heapR,input);
+		}else{
+			heap_push(&heapL,input);
+		}
+	}
+	if(heapL.length==heapR.length){
+		printf("%d %d\n",heap_top(&heapL), heap_top(&heapR));
+	}else{
+		printf("%d\n",heap_top(&heapL));
+	}
+	heap_close(&heapL);
+	heap_close(&heapR);
+	return 0;
+}
---
+++ misc/QSORT.C
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct{
+	size_t length;
+	size_t size;
+	size_t itemSize;
+	void *data;
+}arr_t;
+int arr_init(arr_t *arr, size_t itemSize, size_t size){
+	arr->length=0;
+	arr->size=size;
+	arr->itemSize=itemSize;
+	arr->data=(void*)malloc(itemSize*size);
+	return arr->data ? 1 : 0;
+}
+void arr_close(arr_t *arr){
+	free(arr->data);
+	arr->data=NULL;
+	arr->length=arr->size=arr->itemSize=0;
+}
+int arr_expand(arr_t *arr){
+	size_t sizeNew;
+	void* dataNew;
+	if(arr->length < arr->size){
+		return 1;
+	}
+	sizeNew=arr->size+((arr->size)>>1);
+	dataNew=(void*)realloc(arr->data,(arr->itemSize)*sizeNew);
+	if(!dataNew){
+		return 0;
+	}
+	arr->data=dataNew;
+	arr->size=sizeNew;
+	return 2;
+}
+
+typedef int item_t;
+int arr_push(arr_t *arr, item_t *item){
+	if(!arr_expand(arr)){
+		return 0;
+	}
+	((item_t*)(arr->data))[arr->length]=*item;
+	arr->length++;
+	return 1;
+}
+static void __bubbleSort(item_t *arr, size_t start, size_t end){
+	size_t i,j; item_t temp;
+	for(i=start;i<end-1;i++){
+		for(j=i+1;j<end;j++){
+			if(arr[i]>arr[j]){
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+}
+static void __qsort(item_t *arr, size_t start, size_t end){
+	size_t left, right;
+	item_t pivotVal, temp;
+	if((end-start)<5){
+		__bubbleSort(arr,start,end);
+		return;
+	}
+	pivotVal=arr[start];
+	left=start+1;
+	right=end-1;
+	while(left<right){
+		while(left<end-1 && arr[left]<=pivotVal){
+			left++;
+		}
+		while(right>start && arr[right]>=pivotVal){
+			right--;
+		}
+		if(left>=right){
+			break;
+		}
+		temp=arr[left]; arr[left]=arr[right]; arr[right]=temp;
+	}
+	if(right>start){
+		arr[start]=arr[right]; arr[right]=pivotVal;
+		__qsort(arr,start,right);
+	}
+	if(right<(end-1)){
+		__qsort(arr,right+1,end);
+	}
+}
+void qsort(item_t *arr, size_t length){
+	__qsort(arr,0,length);
+}
+int main(){
+	arr_t arr;
+	unsigned long i;
+	item_t input, *arrData;
+
+	arr_init(&arr,sizeof(item_t),128);
+	while(EOF!=scanf("%d",&input)){
+		arr_push(&arr,&input);
+	}
+	arrData=(item_t*)(arr.data);
+	qsort(arrData,arr.length);
+	for(i=0; i<arr.length; i++){
+		printf("%d\n",arrData[i]);
+	}
+	arr_close(&arr);
+	return 0;
+}
