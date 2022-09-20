d154cccb9efdb75256b4529a61f6cd930adc152d
frank-deng <13382084679@163.com>
2022-09-21 00:45:30 +0800

    BMP2CGA.C supports GW-BASIC PUT image fragment.

--- turbo-c/BMP2CGA.C
+++ turbo-c/BMP2CGA.C
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define MAX_IMAGE_WIDTH (640)
+#define MAX_IMAGE_HEIGHT (200)
+
 typedef struct {
     unsigned short magicNum;
     unsigned long size;
@@ -24,7 +27,8 @@ int main(int argc, char *argv[])
     FILE *src = NULL;
     FILE *dest = NULL;
     bmp_header_t bmpHeader;
-    unsigned short y;
+    unsigned short y, u16val;
+    unsigned short rowBytesSrc, rowBytesTarget;
 
     if (argc < 3) {
         fputs("Usage: bmp2cga.exe BMP_FILE OUTPUT_FILE\n", stderr);
@@ -38,40 +42,63 @@ int main(int argc, char *argv[])
     }
 
     // Checking bmp file
-	fread(&bmpHeader, sizeof(bmp_header_t), 1, src);
+    fread(&bmpHeader, sizeof(bmp_header_t), 1, src);
     if (0x4d42 != bmpHeader.magicNum) {
         fputs("Not a BMP file.\n", stderr);
         return 1;
     }
-    if (1 != bmpHeader.planes){
+    if (1 != bmpHeader.planes) {
         fputs("Invalid BMP file.\n", stderr);
         return 1;
     }
-		if (640 != bmpHeader.width || 200 != bmpHeader.height || 1 != bmpHeader.bpp) {
-        fputs("BMP file must be in 640x200 1bit monochrome format.\n", stderr);
+    if (1 != bmpHeader.bpp) {
+        fputs("BMP file must be in 1bit monochrome format.\n", stderr);
+        return 1;
+    }
+    if (bmpHeader.width > MAX_IMAGE_WIDTH || bmpHeader.height > MAX_IMAGE_HEIGHT) {
+        fputs("BMP file must be smaller than 640x200.\n", stderr);
         return 1;
     }
 
-    // Writing to target file
+    // Prepare target file
     dest = fopen(argv[2], "wb");
     if (NULL == dest) {
         fprintf(stderr, "Failed to open target file %s\n", argv[2]);
         return 1;
     }
     fseek(dest, 0, SEEK_SET);
-    fwrite("\xfd\x00\xb8\x00\x00\x00\x40", sizeof(char), 7, dest);
-    for (y = 0; y < bmpHeader.height; y += 2) {
-        fseek(src, bmpHeader.dataOffset + 80 * (199 - y), SEEK_SET);
-        fread(buf, sizeof(buf), 1, src);
-        fwrite(buf, sizeof(buf), 1, dest);
+
+    // Writing to target file (Fullscreen image)
+    fwrite("\xfd\x00\xb8\x00\x00", sizeof(char), 5, dest);
+    if (bmpHeader.width == MAX_IMAGE_WIDTH && bmpHeader.height == MAX_IMAGE_HEIGHT) {
+        fwrite("\x00\x40", sizeof(char), 2, dest);
+        for (y = 0; y < bmpHeader.height; y += 2) {
+            fseek(src, bmpHeader.dataOffset + 80 * (199 - y), SEEK_SET);
+            fread(buf, sizeof(buf), 1, src);
+            fwrite(buf, sizeof(buf), 1, dest);
+        }
+        fseek(dest, 7+8000+192, SEEK_SET);
+        for (y = 1; y < bmpHeader.height; y += 2) {
+            fseek(src, bmpHeader.dataOffset + 80 * (199 - y), SEEK_SET);
+            fread(buf, sizeof(buf), 1, src);
+            fwrite(buf, sizeof(buf), 1, dest);
+        }
+        fseek(dest, 7+8000+192+8000+192, SEEK_SET);
+    } else {
+        rowBytesSrc = ((bmpHeader.width + 0xf) & (~0xfUL)) >> 3;
+        rowBytesTarget = ((bmpHeader.width + 0x7) & (~0x7UL)) >> 3;
+        u16val = (unsigned short)(rowBytesTarget * bmpHeader.height + 4);
+        fwrite(&u16val, sizeof(unsigned short), 1, dest);
+        u16val = (unsigned short)(bmpHeader.width);
+        fwrite(&u16val, sizeof(unsigned short), 1, dest);
+        u16val = (unsigned short)(bmpHeader.height);
+        fwrite(&u16val, sizeof(unsigned short), 1, dest);
+        for (y = bmpHeader.height; y > 0; y--) {
+            fseek(src, bmpHeader.dataOffset + rowBytesSrc * (y - 1), SEEK_SET);
+            fread(buf, rowBytesTarget, 1, src);
+            fwrite(buf, rowBytesTarget, 1, dest);
+        }
     }
-    fseek(dest, 7+8000+192, SEEK_SET);
-    for (y = 1; y < bmpHeader.height; y += 2) {
-        fseek(src, bmpHeader.dataOffset + 80 * (199 - y), SEEK_SET);
-        fread(buf, sizeof(buf), 1, src);
-        fwrite(buf, sizeof(buf), 1, dest);
-    }
-    fseek(dest, 7+8000+192+8000+192, SEEK_SET);
     fwrite("0x1a", sizeof(char), 1, dest);
 
     // Close files
