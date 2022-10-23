65a680242d31622bd65370e4c8b26c8d75193bc9
frank-deng <13382084679@163.com>
2022-10-23 14:25:43 +0800

    ipcalc update appearance

--- ipcalc/main.c
+++ ipcalc/main.c
@@ -64,31 +64,31 @@ int main(int argc, char *argv[])
 
     if (data.isipv6) {
         uint8_t availHostsBits = 128 - data.netmask;
-        printf("IPv6 Address Compressed: %s\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_COMP));
+        printf("IPv6 Address Compressed:   %s\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_COMP));
         printf("IPv6 Address Uncompressed: %s\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_UNCOMP));
-        printf("IPv6 Address Full: %s\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_FULL));
-        if (availHostsBits > 32) {
-            printf("Available hosts: 2^%u\n", availHostsBits);
+        printf("IPv6 Address Full:         %s\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_FULL));
+        if (availHostsBits > 31) {
+            printf("Available hosts:           2^%u\n", availHostsBits);
         } else if (availHostsBits >= 1) {
-            printf("Available hosts: %lu\n", 1UL << availHostsBits);
+            printf("Available hosts:           %lu\n", 1UL << availHostsBits);
         }
         if (availHostsBits >= 1) {
             ipv6_apply_netmask(&data.ipv6addr, availHostsBits, false);
-            printf("IP Range: %s -\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_FULL));
+            printf("IP Range:                  %s -\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_FULL));
             ipv6_apply_netmask(&data.ipv6addr, availHostsBits, true);
-            printf("          %s\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_FULL));
+            printf("                           %s\n", ipv6_ntop(&(data.ipv6addr), strbuf, IPV6_FORMAT_FULL));
         }
     } else {
         uint32_t wildcard = (1UL << (32 - data.netmask)) - 1;
-        printf("Wildcard: %s = %u\n", ipv4_ntop(wildcard, strbuf), 32 - data.netmask);
-        printf("Netmask: %s = %u\n", ipv4_ntop(~wildcard, strbuf), data.netmask);
+        printf("Wildcard:        %s = %u\n", ipv4_ntop(wildcard, strbuf), 32 - data.netmask);
+        printf("Netmask:         %s = %u\n", ipv4_ntop(~wildcard, strbuf), data.netmask);
         printf("Available hosts: %lu\n", wildcard < 1 ? 0 : wildcard - 1);
-        printf("Network IP: %s\n", ipv4_ntop((data.ipaddr & (~wildcard)), strbuf));
+        printf("Network IP:      %s\n", ipv4_ntop((data.ipaddr & (~wildcard)), strbuf));
         if (data.netmask <= 30) {
-            printf("IP Range: %s - ", ipv4_ntop((data.ipaddr & (~wildcard)) + 1, strbuf));
+            printf("IP Range:        %s - ", ipv4_ntop((data.ipaddr & (~wildcard)) + 1, strbuf));
             puts(ipv4_ntop((data.ipaddr | wildcard) - 1, strbuf));
         }
-        printf("Broadcast IP: %s\n", ipv4_ntop((data.ipaddr | wildcard), strbuf));
+        printf("Broadcast IP:    %s\n", ipv4_ntop((data.ipaddr | wildcard), strbuf));
     }
     return 0;
 }
