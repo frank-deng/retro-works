05dcb286e3c93d3c0b2d56740b9f78e8c740c825
frank-deng <13382084679@163.com>
2022-11-27 13:31:38 +0800

    mkaudiocd.py use self implemented cue writer, use ffmpeg to concat audio files into flac

--- misc/mkaudiocd.py
+++ misc/mkaudiocd.py
@@ -1,48 +1,61 @@
 #!/usr/bin/env python3
-import sys, subprocess, os, multiprocessing, magic, tempfile, getopt, re;
-OUTPUT_CUE='audiocd.cue';
-OUTPUT_AUDIO='audiocd.dat';
+# Install dependencies: pip install audioread
+import sys, subprocess, os, getopt, re, audioread;
+from glob import glob;
 
-opts, args = getopt.getopt(sys.argv[1:], 'o:');
-for k,v in opts:
-    if (k == '-o'):
-        OUTPUT_CUE = v+'.cue';
-        OUTPUT_AUDIO = v+'.dat';
+def sec2timestr(sec):
+    return '%02u:%02u:%02u' % (int(int(sec) / 60), int(sec) % 60, int(75 * (sec % 1)));
 
-files = args;
-tempfiles = [];
+def writeCue(duration, targetCue, targetAudio):
+    totalSec = 0;
+    idx = 0;
+    with open(targetCue, 'w') as f:
+        f.write("FILE \"%s\" %s\n" % (targetAudio, 'FLAC'));
+        for sec in duration:
+            f.write("  TRACK %02u AUDIO\n" % (idx + 1));
+            f.write("    INDEX 01 %s\n" % (sec2timestr(totalSec)));
+            totalSec += sec;
+            idx += 1;
 
-mime = magic.open(magic.MIME);
-mime.load();
-for i in range(len(files)):
-    mime_type = mime.file(files[i]).split(';')[0];
-    if (mime_type in ('audio/mpeg') or re.search(r'\.mp3$', files[i], re.I)):
-        tf = tempfile.NamedTemporaryFile(prefix='%02d-'%(i+1), suffix='.wav', delete=False);
-        tf.close();
-        tempfiles.append(tf.name);
-        plame = subprocess.Popen(['/usr/bin/lame', '--decode', files[i], tf.name]);
-        if (0 == plame.wait()):
-            files[i] = tf.name;
-mime.close();
+if __name__ == '__main__':
+    targetCue='audiocd.cue';
+    targetAudio='audiocd.flac';
+    opts, args = getopt.getopt(sys.argv[1:], 'o:');
+    for k,v in opts:
+        if (k == '-o'):
+            targetCue = v+'.cue';
+            targetAudio = v+'.flac';
 
-pwav = subprocess.Popen(['/usr/bin/shntool', 'join'] + files + ['-o', 'wav', '-O', 'always']);
-pcue = subprocess.Popen(
-    ['/usr/bin/shntool', 'cue']+files,
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE
-);
-stdout, stderr = pcue.communicate();
-sys.stderr.write(stderr.decode('UTF-8'));
-with open(OUTPUT_CUE, 'w') as f:
-    f.write(stdout.decode('UTF-8').replace('joined.wav', OUTPUT_AUDIO).replace('WAVE', 'BINARY'));
+    files = [];
+    duration = [];
+    for arg in args:
+        for fname in glob(arg):
+            try:
+                with audioread.audio_open(fname) as f:
+                    duration.append(f.duration);
+                    files.append(fname);
+            except Exception as e:
+                sys.stderr.write(str(e) + "\n");
+    
+    if len(files) == 0:
+        sys.stderr.write("No audio file found.\n");
+        exit(1);
 
-pwav.wait();
-with open(OUTPUT_AUDIO, 'wb') as fp:
-    pwav = subprocess.Popen(['/usr/bin/shntool', 'cat', '-e', 'joined.wav'], stdout=fp);
-    pwav.wait();
+    try:
+        writeCue(duration, targetCue, targetAudio);
+    except Exception as e:
+        sys.stderr.write(str(e) + "\n");
+        exit(1);
 
-os.unlink('joined.wav');
-for f in tempfiles:
-    os.unlink(f);
-
+    params = ['ffmpeg'];
+    filter_complex = '';
+    idx = 0;
+    for f in files:
+        params.append('-i');
+        params.append(f);
+        filter_complex += '[%u:0]' % idx;
+        idx += 1;
+    filter_complex += 'concat=n=%u:v=0:a=1[out]' % idx;
+    params += ['-filter_complex', filter_complex, '-map', '[out]', targetAudio];
+    proc = subprocess.Popen(params);
+    exit(proc.wait());
