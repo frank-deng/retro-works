a0dd4fcced7f9ee9af8fbc0ccdfb0a4bd1f5a796
frank-deng <13382084679@163.com>
2023-07-31 23:15:30 +0800

    mkphotocd.sh change to mkphotodir.sh

--- misc/mkphotodir.sh
+++ misc/mkphotodir.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 SOURCE="${1}"
 TARGET="${2}"
-TEMPDIR=$(mktemp -d);
 quality=80
 counter=1
 find "${SOURCE}" -maxdepth 1 -type f -iname \*.jpg | sort | while read file; do
 	newfile=DSC$(printf %05d $counter).jpg;
-	convert "${file}" -strip -resize '1280x1280>' -quality $quality "JPEG:${TEMPDIR}/${newfile}"
+	convert "${file}" -strip -resize '1280x1280>' -quality $quality "JPEG:${TARGET}/${newfile}"
 	counter=$((counter+1));
 done;
-genisoimage -o "${TARGET}" "${TEMPDIR}"
-rm -r "${TEMPDIR}"
 
