d80534259d6fd94c05021eb96eb356e4313ce606
frank-deng <13382084679@163.com>
2024-03-01 18:37:13 +0800

    telnet-ssh-adapter.py updated

--- telnet-ppp-server/telnet-ssh-adapter.py
+++ telnet-ppp-server/telnet-ssh-adapter.py
@@ -12,7 +12,9 @@ async def read_line(reader,writer,echo=True,max_len=1024):
     while running:
         char=await asyncio.wait_for(reader.read(1), timeout=login_timeout)
         val=int.from_bytes(char,'little')
-        if 0x08==val and len(res)>0: #Backspace
+        if 0==val: #Connection closed
+            raise BrokenPipeError
+        elif 0x08==val and len(res)>0: #Backspace
             res=res[:-1]
             if(echo):
                 writer.write(b'\x08 \x08')
@@ -44,7 +46,7 @@ async def service_main(reader,writer):
     password=password.decode('UTF-8')
     __loginInfo=loginInfo.get(username)
     if (__loginInfo is None):
-        writer.write(b'Invalid Login.\r\n')
+        writer.write(b'Login Failed.\r\n')
         await writer.drain()
         return
     trans=None
@@ -54,23 +56,11 @@ async def service_main(reader,writer):
         trans.start_client()
         trans.auth_password(__loginInfo.get('user',username), password)
         channel=trans.open_session()
-        channel.get_pty(
-            __loginInfo.get('term','ansi'),
-            __loginInfo.get('cols',80),
-            __loginInfo.get('lines',24)
-        )
+        channel.get_pty(__loginInfo.get('term','ansi'),
+            __loginInfo.get('cols',80), __loginInfo.get('lines',24))
         channel.invoke_shell()
         channel.setblocking(0)
-    except Exception as e:
-        print_exc()
-        writer.write(b'Invalid Login.\r\n')
-        await writer.drain()
-        trans.close()
-        return
-    try:
-        socket_closed=False
-        while trans.is_active() and channel.active and (not channel.closed) and not socket_closed:
-            trans.send_ignore()
+        while trans.is_active() and channel.active and (not channel.closed):
             try:
                 content=channel.recv(1024)
                 if content:
@@ -80,45 +70,45 @@ async def service_main(reader,writer):
             try:
                 content=await asyncio.wait_for(reader.read(1024), timeout=0.01)
                 if not content:
-                    socket_closed=True
+                    raise BrokenPipeError
                 else:
                     channel.send(content);
             except asyncio.TimeoutError:
                 pass
             except socket.timeout:
                 pass
-    except Exception as e:
-        print_exc()
+        writer.write(b'\x1b[1;1f\x1b[2J')
+        await writer.drain()
+    except paramiko.ssh_exception.AuthenticationException:
+        writer.write(b'Login Failed.\r\n')
+        await writer.drain()
     finally:
-        channel.close()
-        trans.close()
-    writer.write(b'\r\nSession closed.\r\n')
-    await writer.drain()
+        if channel is not None:
+            channel.close()
+        if trans is not None:
+            trans.close()
 
 async def service_handler(reader,writer):
     try:
+        writer.write(b'\x1b[1;1f\x1b[2J')
         while True:
             await service_main(reader,writer)
     except asyncio.TimeoutError:
         writer.write(b'\r\n\r\nTimeout!!\r\n')
     except ConnectionResetError:
         pass
+    except BrokenPipeError:
+        pass
+    except Exception as e:
+        print_exc()
     finally:
         writer.close()
         await writer.wait_closed()
-		
-def exception_handler(loop, context):
-    exception = context['exception']
-    if isinstance(exception,BrokenPipeError):
-        return
-    message=context['message']
-    print(f'Task failed, msg={message}, exception={exception}')
 		
 async def main(host,port):
     server=await asyncio.start_server(service_handler,
         host=host,port=port,reuse_address=True,reuse_port=True)
     loop = asyncio.get_event_loop()
-    loop.set_exception_handler(exception_handler)
     for s in (signal.SIGINT, signal.SIGTERM, signal.SIGQUIT):
         loop.add_signal_handler(s, lambda: server.close())
     try:
