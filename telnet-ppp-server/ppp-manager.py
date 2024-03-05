4bb02f2d0c7dab7c15a21ca1c12055cb920679af
frank-deng <13382084679@163.com>
2024-03-05 16:22:51 +0800

    ppp-manager.py use asyncio

--- telnet-ppp-server/ppp-manager.py
+++ telnet-ppp-server/ppp-manager.py
@@ -1,156 +1,162 @@
 #!/usr/bin/env python3
 
+import asyncio,signal,json,hashlib,subprocess,pty,fcntl,os,time
 from traceback import print_exc
-import json,hashlib,subprocess,pty,fcntl,os,time
-from utils import SocketServer,BaseLogin
 
 pppdExec='/usr/sbin/pppd'
+login_timeout=120
+loginInfo={}
+session_info={}
 
-class PPPApp:
-    __process=None;
-    def __init__(self,args):
-        self.__master, self.__slave = pty.openpty();
-        fcntl.fcntl(self.__master, fcntl.F_SETFL, fcntl.fcntl(self.__master, fcntl.F_GETFL) | os.O_NONBLOCK);
-        fcntl.fcntl(self.__slave, fcntl.F_SETFL, fcntl.fcntl(self.__slave, fcntl.F_GETFL) | os.O_NONBLOCK);
-        ptyPath="/proc/%d/fd/%d"%(os.getpid(),self.__slave);
-        self.__process=subprocess.Popen(
-            [
-               pppdExec,
-               ptyPath,
-            ] + args,
-            bufsize=0,
-            start_new_session=True,
-            stdin=self.__slave,
-            stdout=self.__slave,
-            stderr=self.__slave
-        );
+async def read_line(reader,writer,echo=True,max_len=1024):
+    res=b''
+    running=True
+    while running:
+        char=await asyncio.wait_for(reader.read(1), timeout=login_timeout)
+        val=int.from_bytes(char,'little')
+        if 0==val: #Connection closed
+            raise BrokenPipeError
+        elif 0x08==val and len(res)>0: #Backspace
+            res=res[:-1]
+            if(echo):
+                writer.write(b'\x08 \x08')
+                await writer.drain()
+        elif 0x0d==val or 0x0a==val or 0==val: #Finished
+            running=False
+            writer.write(b'\r\n')
+            await writer.drain()
+        elif val>=0x20 and val<=0x7e and len(res)<max_len:
+            res+=val.to_bytes(1,'little')
+            if(echo):
+                writer.write(val.to_bytes(1,'little'))
+                await writer.drain()
+    try:
+        await asyncio.wait_for(reader.read(max_len), timeout=0.01)
+    except asyncio.TimeoutError:
+        pass
+    return res
 
-    def close(self):
-        if self.__process is None:
-            return;
-        os.close(self.__slave);
-        os.close(self.__master);
-        self.__process=None;
-
-    def read(self, data):
-        if self.__process is None:
-            return None;
-        try:
-            os.write(self.__master, data);
-            return True;
-        except Exception as e:
-            print_exc()
-            return None;
-
-    def write(self):
-        if self.__process is None:
-            return None;
-        try:
-            return os.read(self.__master, 65536);
-        except BlockingIOError:
-            return b'';
-        except Exception as e:
-            print_exc()
-            return None;
-
-class LoginHandler(BaseLogin):
-    conn={}
-    __loginInfo={};
-    __app=None;
-    def __init__(self,configFile):
-        super().__init__();
-        self.__running=True
-        with open(configFile, 'r') as f:
-            self.__loginInfo=json.load(f);
-
-    def onLogin(self,_username,_password):
-        username=_username.decode('UTF-8');
-        password=hashlib.sha256(_password).hexdigest();
-        loginInfo=self.__loginInfo.get(username);
-        if (loginInfo is None) or password!=loginInfo['password']:
-            return b'Invalid Login.\r\n';
-        try:
-            self.__username=username;
-            if username in LoginHandler.conn:
-                LoginHandler.conn[username].close()
-                time.sleep(1)
-            self.__app=PPPApp(
-                loginInfo['options'],
-            );
-            LoginHandler.conn[username]=self
-            return b'Success.'+b'\r\n';
-        except Exception as e:
-            print_exc()
-            return b'Invalid Login.\r\n';
-
-    def close(self):
-        self.__running=False
-        if not self.__app:
-            return
-        try:
-            self.__app.close();
-            if self.__username in LoginHandler.conn:
-                del LoginHandler.conn[self.__username]
-        except Exception as e:
-            print_exc()
-        self.__app=None;
-    
-    def read(self,content):
-        if not self.__running:
-            return False
-        if self.__app:
+async def run_ppp_session(args):
+    __master, __slave = pty.openpty()
+    fcntl.fcntl(__master, fcntl.F_SETFL, fcntl.fcntl(__master, fcntl.F_GETFL) | os.O_NONBLOCK)
+    fcntl.fcntl(__slave, fcntl.F_SETFL, fcntl.fcntl(__slave, fcntl.F_GETFL) | os.O_NONBLOCK)
+    ptyPath="/proc/%d/fd/%d"%(os.getpid(),__slave)
+    subprocess.Popen([pppdExec,ptyPath,] + args, bufsize=0, start_new_session=True,
+        stdin=__slave, stdout=__slave, stderr=__slave)
+    try:
+        while True:
+            writer.write(os.read(__master, 1024))
             try:
-                if self.__app.read(content) is not None:
-                    return True;
-            except Exception as e:
-                print_exc()
-            self.close();
-            return False
-        return super().read(content)
+                content=await asyncio.wait_for(reader.read(1024), timeout=0.01)
+                if not content:
+                    raise BrokenPipeError
+                os.write(__master, content)
+            except asyncio.TimeoutError:
+                pass
+    finally:
+        __slave.close()
+        __master.close()
 
-    def write(self):
-        if not self.__running:
-            return None
-        output=b'';
-        if self.__app:
+async def service_main(reader,writer):
+    global loginInfo
+    writer.write(b'\r\nLogin:')
+    await writer.drain()
+    username=await read_line(reader,writer,echo=True,max_len=80)
+    if not username:
+        return True
+    writer.write(b'Password:')
+    await writer.drain()
+    password=await read_line(reader,writer,echo=False,max_len=80)
+    username=username.decode('UTF-8')
+    password=hashlib.sha256(password).hexdigest();
+    __loginInfo=loginInfo.get(username)
+    if __loginInfo is None or password != __loginInfo['password']:
+        writer.write(b'Login Failed.\r\n')
+        await writer.drain()
+        return True
+    elif username in session_info:
+        session_info[username].close()
+        session_info[username]=writer
+    __master, __slave = pty.openpty()
+    fcntl.fcntl(__master, fcntl.F_SETFL, fcntl.fcntl(__master, fcntl.F_GETFL) | os.O_NONBLOCK)
+    fcntl.fcntl(__slave, fcntl.F_SETFL, fcntl.fcntl(__slave, fcntl.F_GETFL) | os.O_NONBLOCK)
+    ptyPath="/proc/%d/fd/%d"%(os.getpid(),__slave)
+    subprocess.Popen([pppdExec,ptyPath,] + __loginInfo['options'], bufsize=0, start_new_session=True,
+        stdin=__slave, stdout=__slave, stderr=__slave)
+    try:
+        while True:
+            writer.write(os.read(__master, 1024))
             try:
-                content=self.__app.write();
-                if content is not None:
-                    return content;
-            except Exception as e:
-                print_exc()
-            self.close();
-            return None
-        output+=super().write()
-        return output
+                content=await asyncio.wait_for(reader.read(1024), timeout=0.01)
+                if not content:
+                    raise BrokenPipeError
+                os.write(__master, content)
+            except asyncio.TimeoutError:
+                pass
+    finally:
+        __slave.close()
+        __master.close()
+    await writer.drain()
+    return False
+
+async def service_handler(reader,writer):
+    try:
+        running=True
+        while running:
+            running=await service_main(reader,writer)
+    except asyncio.TimeoutError:
+        writer.write(b'\r\n\r\nTimeout!!\r\n')
+    except ConnectionResetError:
+        pass
+    except BrokenPipeError:
+        pass
+    except Exception as e:
+        print_exc()
+    finally:
+        writer.close()
+        await writer.wait_closed()
+		
+async def main(host,port):
+    server=await asyncio.start_server(service_handler,
+        host=host,port=port,reuse_address=True,reuse_port=True)
+    loop = asyncio.get_event_loop()
+    for s in (signal.SIGINT, signal.SIGTERM, signal.SIGQUIT):
+        loop.add_signal_handler(s, lambda: server.close())
+    try:
+        async with server:
+            await server.serve_forever()
+    except asyncio.exceptions.CancelledError:
+        pass
+    finally:
+        await server.wait_closed()
 
 if '__main__'==__name__:
-    import argparse;
-    parser = argparse.ArgumentParser();
+    import argparse
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         '--host',
         '-H',
         help='Specify binding host for the telnetd server.',
         default=''
-    );
+    )
     parser.add_argument(
         '--port',
         '-P',
         help='Specify port for the telnetd server.',
         type=int,
         default=23
-    );
+    )
     parser.add_argument(
         '--config',
         '-c',
         help='Specify config file for the telnetd server.',
         default='./ppp.conf'
-    );
+    )
     parser.add_argument(
         '--pppd',
         help='Specify the path of pppd.',
         default='/usr/sbin/pppd'
-    );
+    )
     args = parser.parse_args();
     pppdExec = args.pppd;
 
@@ -158,12 +164,4 @@ if '__main__'==__name__:
     with open(args.config, 'r') as f:
         loginInfo=json.load(f);
 
-    socketServer=SocketServer(args.host,args.port,LoginHandler,(args.config,));
-    try:
-        socketServer.run();
-    except KeyboardInterrupt:
-        pass;
-    finally:
-        socketServer.close();
-    exit(0);
-
+    asyncio.run(main(args.host,args.port))
