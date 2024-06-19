6da26f10fcdde82e648b3cb30403976e8228bc99
frank-deng <13382084679@163.com>
2024-06-19 14:12:53 +0800

    24STAT.C updated

--- C/24STAT.C
+++ C/24STAT.C
@@ -11,6 +11,9 @@
 #define ANS_MIN 0
 #define ANS_MAX 100
 
+static FILE *g_fpcnt=NULL;
+static FILE *g_fpexpr=NULL;
+
 typedef enum {
     OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_MAX
 } op_t;
@@ -81,23 +84,43 @@ static int calc_res(double *n, op_t *op, unsigned int ans)
     }
     return -1;
 }
+static void print_expr(int idx, unsigned int* n0, unsigned int *n, op_t *op, unsigned int ans)
+{
+    const char *expr[]={
+    	"%u%c(%u%c(%u%c%u))=%u","%u%c((%u%c%u)%c%u))=%u","(%u%c(%u%c%u))%c%u=%u",
+        "(%u%c%u)%c(%u%c%u)=%u","((%u%c%u)%c%u)%c%u=%u",
+    };
+    const char opc[]={'+','-','*','/'};
+    fprintf(g_fpexpr,"%u,%u,%u,%u,%u,",n0[0],n0[1],n0[2],n0[3],ans);
+    fprintf(g_fpexpr,expr[idx],n[0],opc[op[0]],n[1],opc[op[1]],n[2],opc[op[2]],n[3],ans);
+    fprintf(g_fpexpr,"\n");
+}
 
-static bool cycle_opers(unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned int ans)
+static bool cycle_opers(unsigned int* n0, unsigned int a, unsigned int b,
+	unsigned int c, unsigned int d, unsigned int ans)
 {
     op_t op[OP_CNT];
+    unsigned int n2[NUM_CNT];
     double n[NUM_CNT];
     n[0]=(double)a;
     n[1]=(double)b;
     n[2]=(double)c;
     n[3]=(double)d;
+    n2[0]=a;
+    n2[1]=b;
+    n2[2]=c;
+    n2[3]=d;
     for(op[0]=OP_ADD; op[0]<OP_MAX; op[0]++){
         for(op[1]=OP_ADD; op[1]<OP_MAX; op[1]++){
             for(op[2]=OP_ADD; op[2]<OP_MAX; op[2]++){
-                res=calc_res(n,op,ans);
+                int res=calc_res(n,op,ans);
                 if(res>=0){
+                    if(NULL!=g_fpexpr){
+                    	print_expr(res,n0,n2,op,ans);
+                    }
                     return true;
                 }
-    		}
+			}
     	}
     }
 finish:
@@ -107,7 +130,11 @@ finish:
 static bool check_ans(unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned int ans)
 {
     unsigned int i,j,k;
-    unsigned int n[]={a,b,c,d};
+    unsigned int n[NUM_CNT];
+    n[0]=a;
+    n[1]=b;
+    n[2]=c;
+    n[3]=d;
     for(i=0;i<NUM_CNT;i++){
         for(j=0;j<NUM_CNT;j++){
             if(i==j){
@@ -117,7 +144,7 @@ static bool check_ans(unsigned int a, unsigned int b, unsigned int c, unsigned i
                 if(i==k || j==k){
                		continue;
             	}
-                if(cycle_opers(n[i],n[j],n[k],n[6-i-j-k],ans)){
+                if(cycle_opers(n,n[i],n[j],n[k],n[6-i-j-k],ans)){
                     return true;
                 }
             }
@@ -159,12 +186,28 @@ unsigned int get_ans_combs(unsigned int ans)
     return cnt;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     unsigned int ans;
+    g_fpcnt=stdout;
+    if(argc>=2){
+        g_fpcnt=fopen(argv[1],"w");
+        if(NULL==g_fpcnt){
+            g_fpcnt=stdout;
+        }
+    }
+    if(argc>=3){
+        g_fpexpr=fopen(argv[2],"w");
+    }
     for(ans=ANS_MIN; ans<=ANS_MAX; ans++){
         unsigned int combs=get_ans_combs(ans);
-        printf("%u,%u\n",ans,combs);
+        fprintf(g_fpcnt,"%u,%u\n",ans,combs);
+    }
+    if(NULL!=g_fpcnt && stdout!=g_fpcnt){
+        fclose(g_fpcnt);
+    }
+    if(NULL!=g_fpexpr){
+        fclose(g_fpexpr);
     }
     return 0;
 }
