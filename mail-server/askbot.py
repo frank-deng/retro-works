17f4b992ec2bfd69661af2b94323d3bcc4021c4f
frank-deng <13382084679@163.com>
2024-07-19 15:04:52 +0800

    Updated emailtest.py, add askbot.py

--- mail-server/askbot.py
+++ mail-server/askbot.py
@@ -1,6 +1,8 @@
 import aiohttp,asyncio,json,sys
 import email
 from email.message import EmailMessage
+from codecs import decode
+from traceback import print_exc
 
 async def getAccessToken(client_id,client_secret):
     url='https://aip.baidubce.com/oauth/2.0/token'
@@ -32,33 +34,49 @@ async def askBot(access_token,question):
             return res.get('result',None)
     return None
 
-async def handler(key,msg):
-    question=None
+def msg_get_data(msg):
     charset=None
+    payload=None
     if msg.is_multipart():
         for part in msg.walk():
-            content_type=part.get_content_type()
-            content_encoding=part.get_content_transfer_encoding()
-            if content_type in {'text/plain','text/html'}:
+            if part.get_content_type() in {'text/plain','text/html'}:
                 charset=part.get_content_charset()
-                question=part.get_payload(decode=True).decode(charset)
+                payload=part.get_payload(decode=True).decode(charset)
     else:
         charset=msg.get_content_charset()
-        question=msg.get_payload(decode=True).decode(charset)
-    if question is None:
-        return
+        payload=msg.get_payload(decode=True).decode(charset)
+    subject=decode(msg['subject'].encode('ascii'), charset)
+    return subject,payload,charset
+    
+def content_append_msg(msg):
+    subject,content,charset=msg_get_data(msg)
+    marker='> '
+    res='\n\n----------'
+    if 'Date' in msg:
+        res+=f"\n{marker}Date: {msg['Date']}"
+    if 'From' in msg:
+        res+=f"\n{marker}From: {msg['From']}"
+    if 'To' in msg:
+        res+=f"\n{marker}To: {msg['To']}"
+    if 'Subject' in msg:
+        res+=f"\n{marker}Subject: {subject}"
+    res+=f"\n{marker}"
+    for line in content.rstrip().split('\n'):
+        res+=f"\n{marker}{line.rstrip()}"
+    return res
+
+async def handler(key,msg):
+    subject,question,charset=msg_get_data(msg)
+    msg_reply = EmailMessage()
+    msg_reply.set_param('charset',charset)
+    msg_reply['From']='niwenwoda@10.0.2.2'
+    msg_reply['To']=msg['From']
+    msg_reply['Subject']="Re: "+msg['Subject']
     access_token=await getAccessToken(key[0],key[1])
     ans=await askBot(access_token,question)
-    payload=ans+'\n\n----------'
-    for line in question.split('\n'):
-        payload+=(f"\n> {line}").rstrip()
-    msg2 = EmailMessage()
-    msg2['From']='niwenwoda@10.0.2.2'
-    msg2['To']=msg['From']
-    msg2['Subject']="Re: "+msg['subject']
-    msg2.set_content(payload.encode(charset),'text','plain',cte='7bit')
-    msg2.set_param('charset',charset)
-    return msg2
+    content=ans+content_append_msg(msg)
+    msg_reply.set_content(content.encode(charset,'ignore'),'text','plain',cte='7bit')
+    return msg_reply
 
 async def run(params,recvQueue,sendQueue):
     while True:
--- mail-server/emailtest.py
+++ mail-server/emailtest.py
@@ -1,42 +1,57 @@
 import email,asyncio
 from email.message import EmailMessage
 from traceback import print_exc
+from codecs import decode
 
-def process_email(msg_raw):
-    msg=email.message_from_bytes(msg_raw)
-    content=None
+def msg_get_data(msg):
     charset=None
+    payload=None
     if msg.is_multipart():
         for part in msg.walk():
-            content_type=part.get_content_type()
-            content_encoding=part.get_content_transfer_encoding()
-            if content_type in {'text/plain','text/html'}:
+            if part.get_content_type() in {'text/plain','text/html'}:
                 charset=part.get_content_charset()
                 payload=part.get_payload(decode=True).decode(charset)
     else:
         charset=msg.get_content_charset()
-        print(msg.get_payload(decode=True),'\n')
         payload=msg.get_payload(decode=True).decode(charset)
+    subject=decode(msg['subject'].encode('ascii'), charset)
+    return subject,payload,charset
+    
+def content_append_msg(msg):
+    subject,content,charset=msg_get_data(msg)
+    marker='> '
+    res='\n\n----------'
+    if 'Date' in msg:
+        res+=f"\n{marker}Date: {msg['Date']}"
+    if 'From' in msg:
+        res+=f"\n{marker}From: {msg['From']}"
+    if 'To' in msg:
+        res+=f"\n{marker}To: {msg['To']}"
+    if 'Subject' in msg:
+        res+=f"\n{marker}Subject: {subject}"
+    res+=f"\n{marker}"
+    for line in content.rstrip().split('\n'):
+        res+=f"\n{marker}{line.rstrip()}"
+    return res
+    
+def process_email(msg_raw):
+    msg=email.message_from_bytes(msg_raw)
+    subject,content,charset=msg_get_data(msg)
     msg_reply = EmailMessage()
-    msg_reply['From']='test@10.0.2.2'
-    msg_reply['To']=msg['From']
-    msg_reply['Subject']="Re: "+msg['subject']
-    print(payload)
-    payload_new="测试成功OK。\n\n----------"
-    for line in payload.split('\n'):
-        payload_new+=f"\n> {line.rstrip()}"
-    msg_reply.set_content(payload_new.encode(charset,'ignore'),'text','plain',cte='7bit')
     msg_reply.set_param('charset',charset)
+    msg_reply['From']=msg['To']
+    msg_reply['To']=msg['From']
+    msg_reply['Subject']="Re: "+msg['Subject']
+    content="测试成功OK。"+content_append_msg(msg)
+    msg_reply.set_content(content.encode(charset,'ignore'),'text','plain',cte='7bit')
     return msg_reply
 
 async def run(params,recvQueue,sendQueue):
     while True:
         msgInfo=await recvQueue.get()
         try:
-            print(msgInfo['msg'],'\n')
             msg_reply=process_email(msgInfo['msg'])
             msg_reply_bytes=msg_reply.as_bytes().replace(b'\n',b'\r\n')
-            print(msg_reply_bytes,'\n')
             sendQueue.put_nowait({
                 'from':'test',
                 'to':msgInfo['from'],
