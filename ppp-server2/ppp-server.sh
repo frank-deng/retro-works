f6fcc90d88db05daed99a4bc913403c1f29ab8ab
frank-deng <13382084679@163.com>
2025-07-26 18:54:57 +0800

    new ppp-server updated

--- ppp-server2/ppp-server.sh
+++ ppp-server2/ppp-server.sh
@@ -7,5 +7,5 @@ iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination :8080
 iptables -t nat -A OUTPUT -d 0.0.0.0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 while ! ifconfig -a|grep -q "inet addr:${IP_ADDR}"; do sleep 1; done;
 dnsmasq "--listen-address=${IP_ADDR}"
-socat TCP-LISTEN:2345,reuseaddr,fork EXEC:'sh -c "/usr/local/sbin/pppd \$(tty)"',pty,stderr,setsid,setpgid &
+socat TCP-LISTEN:2345,reuseaddr,fork EXEC:'sh -c "/usr/local/sbin/pppd \$(tty)"',pty,stderr,setsid &
 
