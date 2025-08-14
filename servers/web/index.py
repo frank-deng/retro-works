a8c76856efd5311451a43eeaa2930bef42f72a14
frank-deng <13382084679@163.com>
2025-08-14 23:30:36 +0800

    servers index fix error

--- servers/web/index.py
+++ servers/web/index.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from aiohttp.web import Request
 from aiohttp.web import Response
 from aiohttp_jinja2 import render_string
@@ -6,20 +7,25 @@ from datetime import datetime
 from web.weather import WeatherData
 
 async def get_weather(config,locid):
+    logger=logging.getLogger(__name__)
     if not locid:
         return None
-    warningColorTable={'蓝色':'#0000ff','黄色':'#a0a000',
-                       '橙色':'#ff8000','红色':'#ff0000'}
-    weatherData=WeatherData(config['web']['heweather_key'])
-    weather=await weatherData.fetch_weather(locid)
-    location=weather['location']
-    location_str=location['adm2']
-    if location['name']!=location['adm2']:
-        location_str+='-'+location['name']
-    weather['location_str']=location_str
-    for item in weather['warning']:
-        item['level_rgb']=warningColorTable.get(item['level'],'#000000')
-    return weather
+    try:
+        warningColorTable={'蓝色':'#0000ff','黄色':'#a0a000',
+                           '橙色':'#ff8000','红色':'#ff0000'}
+        weatherData=WeatherData(config['web']['heweather_key'])
+        weather=await weatherData.fetch_weather(locid)
+        location=weather['location']
+        location_str=location['adm2']
+        if location['name']!=location['adm2']:
+            location_str+='-'+location['name']
+        weather['location_str']=location_str
+        for item in weather['warning']:
+            item['level_rgb']=warningColorTable.get(item['level'],'#000000')
+        return weather
+    except Exception as e:
+        logger.error(e,exc_info=True)
+        return None
 
 async def get_news(newsManager):
     newsList=await newsManager.newsList()
@@ -34,7 +40,7 @@ async def index(req:Request):
     )
 
     context={
-        'dateStr':datetime.now().strftime('%Y年%-m月%-d日'),
+        'dateStr':datetime.now().strftime('%Y年%m月%d日'),
         'links':links,
         'weather':weather,
         'news':news
