4227eea922a850363156d1a49c2f2f5b92d1de95
frank-deng <13382084679@163.com>
2025-09-12 18:41:19 +0800

    SUDOKU.C updated for parsing input file more flexible

--- SUDOKU/SUDOKU.C
+++ SUDOKU/SUDOKU.C
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define HELP_TEXT "Usage:\n    %s file\n    %s map_file file\n"
 #define START_CALC "Calculating Sudoku..."
@@ -8,11 +9,17 @@
 #define INVALID_GROUP_NUM_POS "Invalid group number at line %d, column %d.\n"
 #define GROUP_MALFORMED "Group #%u has %u cells, not 9, please check.\n"
 #define INVALID_NUM_POS "Invalid number at line %d, column %d.\n"
-#define DUP_NUM_POS "Duplicated number detected at line %d, column %d.\n"
+
+#define LINE_TOO_MANY_NUM "Too many numbers at line %u\n"
+#define DUP_NUM_POS "Duplicated number detected at line %u, row %u, column %u.\n"
 #define OPEN_FILE_FAILED "Failed to open Sudoku file.\n"
+
 #define OPEN_JIGSAW_FILE_FAILED "Failed to open Sudoku jigsaw map file.\n"
 #define NO_ANSWER_POS "No ansert at line %d, column %d.\n"
 #define NO_ANSWER "No answer.\n"
+
+typedef unsigned char u8;
+typedef unsigned short u16;
 
 enum{
     E_OK,
@@ -22,14 +29,14 @@ enum{
 };
 
 typedef struct {
-    unsigned char x;
-    unsigned char y;
-    unsigned char grp;
-    unsigned short map;
-    unsigned char last_num;
+    u8 x;
+    u8 y;
+    u8 grp;
+    u8 last_num;
+    u16 map;
 } stack_item_t;
 
-static unsigned char group[9][9] = {
+static u8 group[9][9] = {
     {0,0,0,1,1,1,2,2,2},
     {0,0,0,1,1,1,2,2,2},
     {0,0,0,1,1,1,2,2,2},
@@ -40,7 +47,7 @@ static unsigned char group[9][9] = {
     {6,6,6,7,7,7,8,8,8},
     {6,6,6,7,7,7,8,8,8},
 };
-static unsigned char board[9][9] = {
+static u8 board[9][9] = {
     {0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0},
@@ -52,13 +59,13 @@ static unsigned char board[9][9] = {
     {0,0,0,0,0,0,0,0,0},
 };
 
-static unsigned short mapx[9],mapy[9],mapgrp[9];
+static u16 mapx[9],mapy[9],mapgrp[9];
 static stack_item_t stack[81];
-static unsigned char stack_len=0;
+static u8 stack_len=0;
 
-static unsigned char trailing_zeros(unsigned short n)
+static u8 trailing_zeros(u16 n)
 {
-    unsigned char res=0;
+    u8 res=0;
     if(0==(n&0xff)){
         res+=8;
         n>>=8;
@@ -77,7 +84,7 @@ static unsigned char trailing_zeros(unsigned short n)
     }
     return res;
 }
-static unsigned char next_num(unsigned short map, unsigned char n)
+static u8 next_num(u16 map, u8 n)
 {
     if(0==map){
         return 0;
@@ -91,7 +98,7 @@ static unsigned char next_num(unsigned short map, unsigned char n)
     return 0;
 }
 void printboard(int jigsaw){
-    unsigned char x, y;
+    u8 x, y;
     for (y = 0; y < 9; y++){
         for (x = 0; x < 9; x++){
             printf("%d ", board[y][x]);
@@ -107,7 +114,7 @@ void printboard(int jigsaw){
 }
 int read_jigsaw_map(char *filename)
 {
-    unsigned char x, y, grp_cells[9]={0,0,0,0,0,0,0,0,0}, i;
+    u8 x, y, grp_cells[81], i;
     unsigned int n;
     FILE *fp = fopen(filename, "r");
     if (NULL==fp){
@@ -137,34 +144,43 @@ int read_jigsaw_map(char *filename)
     return E_OK;
 }
 int read_sudoku(char *filename){
-    unsigned char x, y, grp;
-    unsigned short nmap;
+    u8 x=0, y=0, grp, n;
+    u16 nmap, linenum=1;
+    char buf[80],*p;
     stack_item_t *sp=stack;
-    unsigned int n;
     int ret=E_OK;
     FILE *fp = fopen(filename, "r");
     if (NULL==fp){
         fprintf(stderr, OPEN_FILE_FAILED);
         return E_FAIL;
     }
-    for (y = 0; y < 9; y++){
-        for (x = 0; x < 9; x++){
-            fscanf(fp, "%u", &n);
-            if (n > 9) {
-                fprintf(stderr, INVALID_NUM_POS, y+1, x+1);
+    while(fgets(buf,sizeof(buf),fp)!=NULL){
+	if(buf[0]=='#'){
+	    linenum++;
+	    continue;
+	}
+	x=0;
+	for(p=buf; *p!='\0' && (p-buf)<sizeof(buf); p++){
+	    if((*p<'0' || *p>'9') && (*p!='.')){
+                continue;
+	    }
+	    if(x>=9){
+                fprintf(stderr,LINE_TOO_MANY_NUM,linenum);
                 ret=E_INVAL;
                 goto finally_exit;
-            }
-            board[y][x]=n;
-            grp=group[y][x];
-            if (0==n){
+	    }
+	    grp=group[y][x];
+	    if(*p=='0' || *p=='.'){
                 sp->x = x;
                 sp->y = y;
                 sp->grp = grp;
                 sp++;
                 stack_len++;
-            } else {
-                nmap = (((unsigned short)1)<<n);
+	    }else{
+		n=*p-'0';
+		board[y][x]=n;
+		nmap=1;
+		nmap<<=n;
                 if((mapx[x] & nmap)||(mapy[y] & nmap)||(mapgrp[grp] & nmap)){
                     fprintf(stderr, DUP_NUM_POS, y+1, x+1);
                     ret=E_INVAL;
@@ -173,16 +189,25 @@ int read_sudoku(char *filename){
                 mapx[x] |= nmap;
                 mapy[y] |= nmap;
                 mapgrp[grp] |= nmap;
-            }
-        }
+	    }
+	    x++;
+	}
+	if(x>0){
+	    if(x!=9 || y>=9){
+                ret=E_INVAL;
+                goto finally_exit;
+	    }
+	    y++;
+	}
+	linenum++;
     }
 finally_exit:
     fclose(fp);
     return ret;
 }
 int calc_sudoku_step1(){
-    unsigned char x, y, grp, n, i;
-    unsigned short nmap;
+    u8 x, y, grp, n, i;
+    u16 nmap;
     int running=1;
     stack_item_t *sp=stack, *sp0=stack;
     while(running){
@@ -214,8 +239,8 @@ int calc_sudoku_step1(){
     return ((stack_len==0) ? E_OK : E_AGAIN);
 }
 int calc_sudoku_step2(){
-    unsigned char x, y, grp, n, i;
-    unsigned short map;
+    u8 x, y, grp, n, i;
+    u16 map;
     stack_item_t *sp=stack, *stack_tail=stack+stack_len;
     for (i = 0; i < stack_len; i++, sp++) {
         x = sp->x;
@@ -266,7 +291,7 @@ int main(int argc, char *argv[]){
     int res; char *mapfile=NULL, *sudoku_file=NULL;
     if (argc < 2){
 	print_help(argv[0]);
-        exit(1);
+        return 1;
     }
     if(argc > 2){
         mapfile=argv[1];
@@ -275,19 +300,17 @@ int main(int argc, char *argv[]){
         sudoku_file=argv[1];
     }
     if(mapfile!=NULL && E_OK!=read_jigsaw_map(mapfile)){
-        exit(1);
+        return 1;
     }
     if (E_OK != read_sudoku(sudoku_file)){
-        exit(1);
+        return 1;
     }
     res = calc_sudoku_step1();
     if (res == E_AGAIN){
-        fprintf(stderr, START_CALC);
         if(calc_sudoku_step2()!=E_OK){
             fprintf(stderr, NO_ANSWER);
             return 1;
         }
-        fprintf(stderr, END_CALC);
     }else if (res != E_OK) {
         return 1;
     }
