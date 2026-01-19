30562ba54d311d312de22c131e42d68dfc26af6d
frank-deng <13382084679@163.com>
2026-01-19 13:04:35 +0800

    blog-maker updated font processing

--- blog-maker/blog.py
+++ blog-maker/blog.py
@@ -56,22 +56,19 @@ class FontProcessor:
             tag=item.tag
             if tag=='strong':
                 tag='b'
-            elif tag=='code':
-                tag='pre'
             item_new=etree.Element(tag)
             parent_new.append(item_new)
             for key,value in item.attrib.items():
                 item_new.set(key,value)
             if tag in ('pre','code'):
                 item_new.text=item.text
-                item_new.tail=item.tail
                 self.__copy_element(item,item_new)
-                continue
-            if item.text:
-                self.__apply_font(item_new,item.text)
-            self.__process_element(item,item_new)
-            if item.tail:
-                self.__apply_font(item_new,item.tail)
+            else:
+                if item.text and item.text.strip():
+                    self.__apply_font(item_new,item.text)
+                self.__process_element(item,item_new)
+            if item.tail and item.tail.strip():
+                self.__apply_font(parent_new,item.tail)
 
     def apply_font(self,content,singleLine=False):
         old_tree=html.fromstring("<html>"+content+"</html>")
