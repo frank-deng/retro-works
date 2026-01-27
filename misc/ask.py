431e0a087e9b3ccc12fb8a4969c796ca9558a786
frank-deng <13382084679@163.com>
2026-01-27 23:45:25 +0800

    ask.py use file for multi-round dialogue

--- misc/ask.py
+++ misc/ask.py
@@ -6,34 +6,51 @@ import aiohttp
 import click
 
 class AskBot:
+    def __init__(self, outfile):
+        self.__outfile=outfile
+        
     async def _ondata(self,chunk):
         raise NotImplementedError('_ondata() method must be implemented')
         
     async def _handleResponse(self,response):
         target=sys.stdout.buffer
         proc=None
-        if sys.stdout.isatty():
-            try:
-                proc=subprocess.Popen(['less','-FRX'],stdin=subprocess.PIPE)
-                target=proc.stdin
-            except Exception as e:
-                print(e,file=sys.stderr)
-        async for chunk in response.content:
-            text=await self._ondata(chunk)
-            if text is None:
-                continue
-            target.write(text.encode('utf-8'))
-            target.flush()
-        if proc is not None:
-            proc.stdin.close()
-            proc.wait()
+        fp=None
+        try:
+            fp=open(self.__outfile,'a',encoding='utf8')
+            fp.write('\n'+('-'*50)+'\n')
+            if sys.stdout.isatty():
+                try:
+                    proc=subprocess.Popen(['less','-FRX'],stdin=subprocess.PIPE)
+                    target=proc.stdin
+                except Exception as e:
+                    print(e,file=sys.stderr)
+            async for chunk in response.content:
+                text=await self._ondata(chunk)
+                if text is None:
+                    continue
+                target.write(text.encode('utf-8'))
+                target.flush()
+                if fp is not None:
+                    fp.write(text)
+                    fp.flush()
+            fp.write('\n'+('='*50)+'\n')
+        except Exception as e:
+            click.secho(e,fg='red',err=True)
+        finally:
+            if fp is not None:
+                fp.close()
+            if proc is not None:
+                proc.stdin.close()
+                proc.wait()
 
-    async def ask(self,question):
+    async def ask(self,dialogue):
         raise NotImplementedError('ask() method must be implemented')
 
 
 class AskBotDeepSeek(AskBot):
-    def __init__(self):
+    def __init__(self, outfile):
+        super().__init__(outfile)
         env_key='DEEPSEEK_KEY'
         self.__key=os.environ.get(env_key)
         if not self.__key:
@@ -46,7 +63,7 @@ class AskBotDeepSeek(AskBot):
         data=json.loads(chunk_str)
         return data['choices'][0]['delta']['content']
         
-    async def ask(self,question):
+    async def ask(self,dialogue):
         headers={
             'Content-Type': 'application/json',
             'Accept': 'application/json',
@@ -56,13 +73,16 @@ class AskBotDeepSeek(AskBot):
             "model": "deepseek-chat",
             "top_p": 0.01,
             "stream": True,
-            "messages": [
-                {
-                    'role':'user',
-                    'content':question,
-                }
-            ],
+            "messages": [],
         }
+        for idx in range(len(dialogue)):
+            role='user'
+            if idx&1:
+                role='assistant'
+            jsonData['messages'].append({
+                'role':role,
+                'content':dialogue[idx]
+            })
         async with aiohttp.ClientSession() as session:
             async with session.post('https://api.deepseek.com/chat/completions',
                                     headers=headers,
@@ -73,7 +93,8 @@ class AskBotDeepSeek(AskBot):
 
 
 class AskBotErine(AskBot):
-    def __init__(self):
+    def __init__(self, outfile):
+        super().__init__(outfile)
         env_key='ERINE_KEY'
         self.__key=os.environ.get(env_key)
         if not self.__key:
@@ -85,7 +106,7 @@ class AskBotErine(AskBot):
             return None
         return json.loads(chunk_str).get('result')
         
-    async def ask(self,question):
+    async def ask(self,dialogue):
         client_id,client_secret=self.__key.split(':')
         data={
             'grant_type':'client_credentials',
@@ -102,16 +123,20 @@ class AskBotErine(AskBot):
             raise RuntimeError('Failed to get access token.')
         url=f"https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token={access_token}"
         jsonData={
-            'messages':[
-                {
-                    'role':'user',
-                    'content':question,
-                    'temperature':0.01,
-                    'top_p':0,
-                }
-            ],
+            'messages':[],
             'stream':True,
         }
+        
+        for idx in range(len(dialogue)):
+            role='user'
+            if idx&1:
+                role='assistant'
+            jsonData['messages'].append({
+                'role':role,
+                'content':dialogue[idx],
+                'temperature':0.01,
+                'top_p':0
+            })
         async with aiohttp.ClientSession() as session:
             async with session.post(url,json=jsonData) as response:
                 await self._handleResponse(response)
@@ -122,11 +147,30 @@ MODELS={
     'erine':AskBotErine
 }
 
-async def async_main(model,question):
-    if model not in MODELS:
-        raise RuntimeError(f'Unsupported model "{model}".')
-    askBot=MODELS[model]()
-    await askBot.ask(question)
+
+def parse_file(fpath):
+    if not os.path.isfile(fpath):
+        raise FileNotFoundError(f'{fpath} not found or not a file.')
+    res=[]
+    question=True
+    with open(fpath,'r',encoding='utf8') as fp:
+        content=''
+        for line in fp:
+            if question and re.match(r'^[-]{40,}\r?\n$',line):
+                res.append(content.strip())
+                content=''
+                question=False
+            elif not question and re.match(r'^[=]{40,}\r?\n$',line):
+                res.append(content.strip())
+                content=''
+                question=True
+            else:
+                content+=line
+        content=content.strip()
+        if content:
+            res.append(content)
+    return res
+
 
 @click.command(context_settings={
     'help_option_names': ['-h', '--help', '-?'],
@@ -134,31 +178,19 @@ async def async_main(model,question):
 })
 @click.option('--model', '-m', default='deepseek', show_default=True,
               help='Model to use (deepseek, erine, etc.)')
-@click.argument('question', nargs=-1, required=False)
+@click.argument('file', required=True)
 @click.pass_context
-def main(ctx,model,question):
-    """Examples:
-
-\b
-ask.py question
-ask.py <<< question
-echo question | ask.py
-ask.py < question.txt
-cat question.txt | ask.py
-ask.py question < question.txt
-"""
-    content=' '.join(question)
-    if sys.stdin.isatty() and not content.strip():
-        click.echo(ctx.get_help(),err=True)
-        ctx.exit(1)
-    if not sys.stdin.isatty():
-        try:
-            while True:
-                content+=input().strip()+'\n'
-        except EOFError:
-            pass
+def main(ctx,model,file):
+    """For VIM users, use :!python ask.py %"""
     try:
-        asyncio.run(async_main(model,content))
+        dialogue=parse_file(file)
+        if (len(dialogue)&1)==0:
+            click.secho('Question not found.',fg='yellow',err=True)
+            ctx.exit(1)
+        if model not in MODELS:
+            raise RuntimeError(f'Unsupported model "{model}".')
+        askBot=MODELS[model](file)
+        asyncio.run(askBot.ask(dialogue))
     except BrokenPipeError:
         pass
     except KeyboardInterrupt:
@@ -173,3 +205,4 @@ ask.py question < question.txt
 
 if '__main__'==__name__:
     main()
+
