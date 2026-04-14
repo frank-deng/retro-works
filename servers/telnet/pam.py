107a23d04b005093668602374a03f057712ed962
frank-deng <13382084679@163.com>
2026-04-14 23:48:06 +0800

    telnet server add pam module

--- servers/telnet/pam.py
+++ servers/telnet/pam.py
@@ -1,30 +1,32 @@
 import asyncio
 import os
 import sys
+import pam
+import pamela
 import pwd
 import pty
 import fcntl
 import termios
 import struct
-import hashlib
 from util.tcpserver import TCPServer
 from telnet import login
 from telnet import ProcessHandler
 
 
-class TermuxHandler(ProcessHandler):
-    def __init__(self,reader,writer,username,*,buf_size=4096,shell='/bin/bash',
-                 term='ansi'):
+class UserShellHandler(ProcessHandler):
+    def __init__(self,reader,writer,username,*,buf_size=4096,term='ansi'):
         super().__init__(reader,writer,buf_size=buf_size)
         self.__username=username
-        self.__shell=shell
+        self.__userinfo=pwd.getpwnam(username)
         self.__term=term
 
     async def create_subprocess_exec(self,master_fd,slave_fd):
         env={
-            'USER':self.__username,
-            'SHELL':self.__shell,
+            'USER':self.__userinfo.pw_name,
+            'HOME':self.__userinfo.pw_dir,
+            'SHELL':self.__userinfo.pw_shell,
             'TERM':self.__term,
+            'PATH':'/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
         }
         fcntl.ioctl(slave_fd,termios.TIOCSWINSZ,struct.pack('HHHH',24,80,0,0))
         pid=os.fork()
@@ -36,46 +38,37 @@ class TermuxHandler(ProcessHandler):
             os.dup2(slave_fd,1)
             os.dup2(slave_fd,2)
             os.close(slave_fd)
+            pamela.open_session(self.__userinfo.pw_name)
+            os.setgid(self.__userinfo.pw_gid)
+            os.setuid(self.__userinfo.pw_uid)
             for key,value in env.items():
                 os.environ[key]=value
-            cwd=os.environ.get('HOME',None)
-            if cwd is not None:
-                os.chdir(cwd)
-            os.execl(self.__shell,self.__shell)
+            os.chdir(self.__userinfo.pw_dir)
+            os.execl(self.__userinfo.pw_shell,self.__userinfo.pw_shell,'--login')
             os._exit(1)
 
 
-class TelnetServerTermux(TCPServer):
-    def __init__(self,config):
+class TelnetServerPAM(TCPServer):
+    def __init__(self,port,*,host,max_conn):
         self._config=config[self.__class__.__name__]
         super().__init__(self._config['port'],
                          host=self._config.get('host','127.0.0.1'),
                          max_conn=self._config.get('max_connection'))
 
     async def handler(self,reader,writer):
-        writer.write(b'\xFF\xFD\x22\xFF\xFB\x01\xFF\xFB\x00\xFF\xFD\x00\r\n')
-        await writer.drain()
-        try:
-            while True:
-                dt=await asyncio.wait_for(reader.read(1000),timeout=0.1)
-                self.logger.debug(dt)
-        except asyncio.TimeoutError:
-            pass
         login_failed_count=0
         while login_failed_count<3:
             username,password=await login(reader,writer)
             if username is None or password is None:
                 break
-            if not username:
-                continue
-            if username.decode('ascii')!=self._config['username'] or \
-                hashlib.sha256(password).hexdigest()!=self._config['password']:
+            p=pam.pam()
+            if not p.authenticate(username,password):
                 login_failed_count+=1
                 writer.write(b'Login Failed.\r\n')
                 await asyncio.gather(writer.drain(),asyncio.sleep(1))
                 continue
             login_failed_count=0
-            async with TermuxHandler(reader,writer,self._config['username'],
+            async with UserShellHandler(reader,writer,username,
                 buf_size=self._config.get('buf_size',4096),
                 shell=self._config.get('shell','/bin/bash'),
                 term=self._config.get('term','ansi')):
