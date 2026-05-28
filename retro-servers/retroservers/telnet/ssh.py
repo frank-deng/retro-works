0e2064605b336dc52b8545ac5366f3a1f5e0e390
frank-deng <13382084679@163.com>
2026-05-28 17:10:27 +0800

    telnet ssh server updated

--- retro-servers/retroservers/telnet/ssh.py
+++ retro-servers/retroservers/telnet/ssh.py
@@ -7,105 +7,105 @@ from .util import TelnetServer
 
 class SSHHandler(Logger):
     def __init__(self,reader,writer,config,username,password):
-        self.__conn=None
-        self.__session=None
-        self.__channel=None
-        self.__tasks=None
-        self.__loop=asyncio.get_running_loop()
-        self.__queue=asyncio.Queue()
-        self.__reader,self.__writer=reader,writer
-        self.__config=config
-        self.__username=username
-        self.__password=password
-        self.__buf_size=config.get('buf_size',4096)
+        self._conn=None
+        self._session=None
+        self._channel=None
+        self._tasks=None
+        self._loop=asyncio.get_running_loop()
+        self._queue=asyncio.Queue()
+        self._reader,self._writer=reader,writer
+        self._config=config
+        self._username=username
+        self._password=password
+        self._buf_size=config.get('buf_size',4096)
 
     async def __aenter__(self):
         try:
-            self.__conn = await asyncssh.connect(
-                host=self.__config['ssh_host'],
-                port=self.__config.get('ssh_port',22),
-                username=self.__username,
-                password=self.__password,
+            self._conn = await asyncssh.connect(
+                host=self._config['ssh_host'],
+                port=self._config.get('ssh_port',22),
+                username=self._username,
+                password=self._password,
                 known_hosts=None
             )
-            self.__password=None
-            self.__channel,self.__session=await self.__conn.create_session(
+            self._password=None
+            self._channel,self._session=await self._conn.create_session(
                 asyncssh.SSHClientSession,
-                term_type=self.__config.get('term','ansi'),
-                term_size=(self.__config.get('columns',80),
-                           self.__config.get('rows',24)),
+                term_type=self._config.get('term','ansi'),
+                term_size=(self._config.get('columns',80),
+                           self._config.get('rows',24)),
                 encoding=None
             )
-            self.__session.data_received=self.__ssh_read
-            self.__tasks=(
-                asyncio.create_task(self.__ssh_to_tcp()),
-                asyncio.create_task(self.__tcp_to_ssh())
+            self._session.data_received=self._ssh_read
+            self._tasks=(
+                asyncio.create_task(self._ssh_to_tcp()),
+                asyncio.create_task(self._tcp_to_ssh())
             )
         except Exception:
-            await self.__cleanup()
+            await self._cleanup()
             raise
 
     async def __aexit__(self,exc_type,exc_val,exc_tb):
         try:
-            if self.__tasks is not None:
-                await asyncio.wait(self.__tasks,return_when=asyncio.FIRST_COMPLETED)
+            if self._tasks is not None:
+                await asyncio.wait(self._tasks,return_when=asyncio.FIRST_COMPLETED)
         except asyncio.CancelledError:
             pass
         finally:
-            await self.__cleanup()
+            await self._cleanup()
 
-    async def __cleanup(self):
+    async def _cleanup(self):
         try:
-            if self.__tasks is not None:
-                for task in self.__tasks:
+            if self._tasks is not None:
+                for task in self._tasks:
                     if not task.done():
                         task.cancel()
-                await asyncio.gather(*self.__tasks)
-            if self.__conn is not None:
-                self.__conn.close()
-                await self.__conn.wait_closed()
+                await asyncio.gather(*self._tasks)
+            if self._conn is not None:
+                self._conn.close()
+                await self._conn.wait_closed()
         except Exception:
             raise
 
-    def __ssh_read(self,data,_):
+    def _ssh_read(self,data,_):
         try:
             if not data:
-                self.__loop.call_soon(self.__queue.put_nowait,b"")
+                self._loop.call_soon(self._queue.put_nowait,b"")
                 return
-            self.__queue.put_nowait(data)
+            self._queue.put_nowait(data)
         except asyncio.CancelledError:
-            self.__queue.put_nowait(b"")
+            self._queue.put_nowait(b"")
         except Exception as e:
             self.logger.error(e,exc_info=True)
-            self.__queue.put_nowait(b"")
+            self._queue.put_nowait(b"")
 
-    async def __ssh_to_tcp(self):
+    async def _ssh_to_tcp(self):
         try:
             while True:
-                data=await self.__queue.get()
+                data=await self._queue.get()
                 if not data:
                     break
-                self.__writer.write(data)
-                await self.__writer.drain()
+                self._writer.write(data)
+                await self._writer.drain()
         except (ConnectionResetError,asyncio.CancelledError):
             pass
         except Exception as e:
             self.logger.error(e,exc_info=True)
 
-    async def __tcp_to_ssh(self):
+    async def _tcp_to_ssh(self):
         try:
             while True:
-                data=await self.__reader.read(self.__buf_size)
+                data=await self._reader.read(self._buf_size)
                 if not data:
                     break
-                self.__channel.write(data)
+                self._channel.write(data)
         except (ConnectionResetError,asyncio.CancelledError,BrokenPipeError):
             pass
         except Exception as e:
             self.logger.error(e,exc_info=True)
         finally:
-            self.__channel.close()
-            await self.__channel.wait_closed()
+            self._channel.close()
+            await self._channel.wait_closed()
 
 
 class TelnetServerSSHInstance(TelnetServer):
@@ -122,7 +122,7 @@ class TelnetServerSSHInstance(TelnetServer):
                                   username,password):
                 pass
             return True
-        except asyncssh.misc.PermissionDenied:
+        except (asyncssh.misc.PermissionDenied,ConnectionRefusedError):
             return False
 
 
