3afd2706877e8eb190bebbcc86c1d86a8a4a728e
frank-deng <13382084679@163.com>
2026-06-04 11:02:45 +0800

    httpfileserver add basic auth

--- retro-servers/retroservers/httpfileserver.py
+++ retro-servers/retroservers/httpfileserver.py
@@ -1,5 +1,7 @@
 import asyncio
 import os
+import base64
+import hashlib
 from aiohttp import web
 from aiohttp import web_exceptions
 from aiohttp.web import HTTPFound
@@ -23,6 +25,42 @@ async def _handler(req):
     raise web.HTTPNotFound()
 
 
+async def basic_auth_proc(req:web.Request):
+    username_check=req.app['config'].get('username')
+    password_hash=req.app['config'].get('password')
+    # Disable auth by not setting username and password
+    if not username_check or not password_hash:
+        return True
+    auth_header = req.headers.get('Authorization')
+    if not auth_header:
+        return False
+    try:
+        scheme, credentials = auth_header.split(' ', 1)
+        if scheme.lower() != 'basic':
+            return False
+        username, password = base64.b64decode(credentials).decode().split(':', 1)
+        if username!=username_check:
+            return False
+        password_hash_in=hashlib.sha256(password.encode('iso8859-1',errors='ignore')).hexdigest()
+        if password_hash_in!=password_hash:
+            return False
+        req['user'] = username
+        return True
+    except (ValueError, UnicodeDecodeError):
+        return False
+
+
+@web.middleware
+async def basic_auth_middleware(req:web.Request,handler):
+    if not await basic_auth_proc(req):
+        raise web.HTTPUnauthorized(
+            headers={'WWW-Authenticate': 'Basic realm="Authentication required"'},
+            text='Authentication required'
+        )
+    else:
+        return await handler(req)
+
+
 class HTTPFileServer(Logger):
     def __init__(self,config):
         self._runner=None
@@ -30,7 +68,7 @@ class HTTPFileServer(Logger):
         self._config=config[self.__class__.__name__]
         self._host=self._config.get('host','0.0.0.0')
         self._port=self._config.get('port',80)
-        self._app=web.Application()
+        self._app=web.Application(middlewares=[basic_auth_middleware])
         self._app['config']=self._config
         self._app.router.add_route('GET','/{path:.*}',_handler)
 
