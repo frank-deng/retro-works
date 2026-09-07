8c6ce3ae14ef8cedfeebcc762f080c046d7dfe7f
frank-deng <13382084679@163.com>
2026-09-07 13:53:44 +0800

    testcase for atoi atoui itoa uitoa updatex

--- lab/test.asm
+++ lab/test.asm
@@ -35,39 +35,34 @@ jne test_failed
 inc bx
 jnz test_nums_cycle
 
-;Test u16
-lea bx,[test_u16_list]
-test_u16_cycle:
-mov ax,[bx].VAL
-call uitoa
-cmp cx,[bx].LEN
-jne test_failed
-call atoui
+call test_u16
 jc test_failed
-cmp ax,[bx].VAL
-jne test_failed
-call cmp_str
-jne test_failed
-add bx,SIZE TestCase
-cmp bx,offset test_u16_list_end
-jb test_u16_cycle
+call test_s16
+jc test_failed
 
-;Test s16
-lea bx,[test_s16_list]
-test_s16_cycle:
-mov ax,[bx].VAL
-call itoa
-cmp cx,[bx].LEN
-jne test_failed
+;Test u16 abnormal
+lea bx,[test_u16_abnormal]
+test_u16_abnormal_cycle:
+mov cx,[bx].LEN
+lea si,[bx].STR
+call atoui
+jnc test_failed
 call atoi
-jc test_failed
-cmp ax,[bx].VAL
-jne test_failed
-call cmp_str
-jne test_failed
+jnc test_failed
 add bx,SIZE TestCase
-cmp bx,offset test_s16_list_end
-jb test_s16_cycle
+cmp bx,offset test_u16_abnormal_end
+jb test_u16_abnormal_cycle
+
+;Test s16 abnormal
+lea bx,[test_s16_abnormal]
+test_s16_abnormal_cycle:
+mov cx,[bx].LEN
+lea si,[bx].STR
+call atoi
+jnc test_failed
+add bx,SIZE TestCase
+cmp bx,offset test_s16_abnormal_end
+jb test_s16_abnormal_cycle
 
 test_finish:
 mov ah,09h
@@ -83,15 +78,66 @@ mov bx,1
 mov ah,40h
 int 21h
 
-cmp_str:
+test_s16:
+lea bx,[test_s16_list]
+test_s16_cycle:
+mov ax,[bx].VAL
+call itoa
+call check_res
+jc test_s16_failed
+call atoi
+jc test_s16_failed
+call check_res
+jc test_s16_failed
+add bx,SIZE TestCase
+cmp bx,offset test_s16_list_end
+jb test_s16_cycle
+clc
+ret
+test_s16_failed:
+stc
+ret
+
+test_u16:
+lea bx,[test_u16_list]
+test_u16_cycle:
+mov ax,[bx].VAL
+call uitoa
+call check_res
+jc test_u16_failed
+call atoui
+jc test_u16_failed
+call check_res
+jc test_u16_failed
+add bx,SIZE TestCase
+cmp bx,offset test_u16_list_end
+jb test_u16_cycle
+clc
+ret
+test_u16_failed:
+stc
+ret
+
+check_res:
+push cx
 push si
 push di
+cmp ax,[bx].VAL
+jne check_res_failed
+cmp cx,[bx].LEN
+jne check_res_failed
 lea si,[bx].STR
 cld
 repe cmpsb
+jne check_res_failed
+check_res_finish:
 pop di
 pop si
+pop cx
 ret
+check_res_failed:
+stc
+jmp check_res_finish
 
 mov ah,09h
 lea dx,[test_failed_str]
@@ -116,6 +162,18 @@ TestCase<2,"-1",-1>
 TestCase<6,"-32767",-32767>
 TestCase<6,"-32768",-32768>
 test_s16_list_end:
+test_u16_abnormal:
+TestCase<3,"1aa",0>
+TestCase<6,"123456",0>
+TestCase<6,"100000",0>
+TestCase<5,"65536",0>
+test_u16_abnormal_end:
+test_s16_abnormal:
+TestCase<5,"32768",0>
+TestCase<5,"65535",0>
+TestCase<6,"-32769",0>
+TestCase<6,"-65535",0>
+test_s16_abnormal_end:
 buf db BUFLEN DUP(?)
 
 end start
