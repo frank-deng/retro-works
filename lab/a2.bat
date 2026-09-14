5f9e0864e4e90a02b92b2f4a84a3da2c6ed92f3d
frank-deng <13382084679@163.com>
2026-09-14 12:53:53 +0800

    CNUM.ASM graduated

--- lab/a2.bat
+++ lab/a2.bat
@@ -9,4 +9,30 @@ cnum.com 01200000000>>cnum.txt
 cnum.com 01200010000>>cnum.txt
 cnum.com 01200002000>>cnum.txt
 cnum.com 00>>cnum.txt
+cnum.com 01>>cnum.txt
+cnum.com 010>>cnum.txt
+cnum.com 013>>cnum.txt
+cnum.com 020>>cnum.txt
+cnum.com 024>>cnum.txt
+cnum.com 0100>>cnum.txt
+cnum.com 0102>>cnum.txt
+cnum.com 0192>>cnum.txt
+cnum.com 0190>>cnum.txt
+cnum.com 01000>>cnum.txt
+cnum.com 01001>>cnum.txt
+cnum.com 01020>>cnum.txt
+cnum.com 01024>>cnum.txt
+cnum.com 01200>>cnum.txt
+cnum.com 01209>>cnum.txt
+cnum.com 01249>>cnum.txt
+cnum.com 010000>>cnum.txt
+cnum.com 010002>>cnum.txt
+cnum.com 011002>>cnum.txt
+cnum.com 0100000>>cnum.txt
+cnum.com 01000000>>cnum.txt
+cnum.com 01040000>>cnum.txt
+cnum.com 013040009>>cnum.txt
+cnum.com 12345678912345678>>cnum.txt
+cnum.com 1234567812345678>>cnum.txt
+
 
