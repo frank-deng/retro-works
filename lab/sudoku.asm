408bb5b0062d4c3ad65a4fbe0d42d89d517139d7
frank-deng <13382084679@163.com>
2026-09-15 21:15:02 +0800

    sudoku.asm start solve2

--- lab/sudoku.asm
+++ lab/sudoku.asm
@@ -17,6 +17,10 @@ call init_map
 jc error_dupnum
 call solve1
 jc no_answer
+cmp [cell_info_count],0
+je sudoku_finished
+call solve2
+sudoku_finished:
 call print_map
 
 mov ax,04c00h
@@ -133,7 +137,7 @@ push dx
 push si
 push di
 lea di,[xygmaps]
-mov cx,27
+mov cx,(xygmaps_end-xygmaps)/2
 xor ax,ax
 cld
 rep stosw
@@ -292,9 +296,168 @@ not ax
 and ax,03feh
 ret
 
+solve2:
+push bp
+mov bp,sp
+push ax
+push bx
+push cx
+push dx
+push si
+push di
+mov cx,[cell_info_count]
+lea si,[cell_info]
+init_solve2_loop:
+xor bh,bh
+mov bl,byte ptr[si].X
+shl bl,1
+mov ax,[xmap+bx]
+mov bl,byte ptr[si].Y
+shl bl,1
+or ax,[ymap+bx]
+mov bl,byte ptr[si].GRP
+shl bl,1
+or ax,[gmap+bx]
+not ax
+and ax,03feh
+mov word ptr[si].MAP,ax
+add si,SIZE CellInfo
+loop init_solve2_loop
+lea di,[xygmaps]
+mov cx,(xygmaps_end-xygmaps)/2
+xor ax,ax
+cld
+rep stosw
+
+mov ax,[cell_info_count]
+mov bx,SIZE CellInfo
+mul bx
+mov di,ax
+solve2_proc:
+call get_min_cell
+jc solve2_skip_push
+dec di
+dec di
+mov [di],bx
+solve2_skip_push:
+xor ax,ax
+solve2_proc_inner:
+mov bx,[di]
+mov ah,[bx+cell_info].Y
+mov al,[bx+cell_info].X
+mov ch,[bx+cell_info].GRP
+xor bh,bh
+mov bl,ah
+shl bl,1
+shl bl,1
+shl bl,1
+add bl,ah
+add bl,al
+mov cl,byte ptr[board+bx]
+test cl,cl
+jz solve2_skip_wmap1
+mov dx,1
+shl dx,cl
+not dx
+xor bh,bh
+mov bl,al
+shl bl,1
+and [bx+xmap],dx
+mov bl,ah
+shl bl,1
+and [bx+ymap],dx
+mov bl,ch
+shl bl,1
+and [bx+gmap],dx
+solve2_skip_wmap1:
+
+jmp solve2_proc_inner
+cmp di,0
+jle solve2_proc
+solve2_proc_end:
+cld
+solve2_exit:
+pop di
+pop si
+pop dx
+pop cx
+pop bx
+pop ax
+mov sp,bp
+pop bp
+ret
+solve2_noans:
+stc
+jmp solve2_exit
+
+get_min_cell:
+push bp
+mov bp,sp
+sub sp,4
+push ax
+push cx
+push dx
+push si
+lea si,[cell_info]
+mov [bp-4],si
+mov cx,[cell_info_count]
+mov word ptr[bp-2],0ffh
+get_min_cell_loop:
+mov al,byte ptr[si].X
+mov ah,byte ptr[si].Y
+xor bh,bh
+mov bl,ah
+shl bl,1
+shl bl,1
+shl bl,1
+add bl,ah
+add bl,al
+mov bl,byte ptr[board+bx]
+test bl,bl
+jz get_min_cell_continue
+xor bh,bh
+mov bl,al
+shl bl,1
+mov dx,[xmap+bx]
+mov bl,ah
+shl bl,1
+or dx,[ymap+bx]
+mov bl,byte ptr[si].GRP
+shl bl,1
+or dx,[gmap+bx]
+not dx
+and dx,word ptr[si].MAP
+jz get_min_cell_fail
+xor al,al
+get_min_cell_count_elem:
+inc al
+mov bx,dx
+dec bx
+and dx,bx
+jnz get_min_cell_count_elem
+cmp al,[bp-2]
+jge get_min_cell_continue
+mov [bp-2],al
+mov [bp-4],si
+get_min_cell_continue:
+add si,SIZE CellInfo
+loop get_min_cell_loop
+mov bx,[bp-4]
+clc
+get_min_cell_end:
+pop si
+pop dx
+pop cx
+pop ax
+mov sp,bp
+pop bp
+ret
+get_min_cell_fail:
+stc
+jmp get_min_cell_end
+
 dupnum_str db "Duplicated number detected.",0dh,0ah,'$'
 noans_str db "No answer.",0dh,0ah,'$'
-ALIGN 2
 group_map:
 db 0,0,0,1,1,1,2,2,2
 db 0,0,0,1,1,1,2,2,2
@@ -305,23 +468,33 @@ db 3,3,3,4,4,4,5,5,5
 db 6,6,6,7,7,7,8,8,8
 db 6,6,6,7,7,7,8,8,8
 db 6,6,6,7,7,7,8,8,8
-ALIGN 2
 board:
-db 0,1,0,0,0,5,3,9,0
-db 5,0,2,0,0,0,0,0,8
-db 9,0,0,1,3,0,0,5,0
-db 8,0,0,0,4,0,5,0,0
-db 0,0,3,5,6,8,7,0,0
-db 0,0,1,0,2,0,0,0,9
-db 0,8,0,0,5,7,0,0,4
-db 1,0,0,0,0,0,2,0,3
-db 0,7,9,3,0,0,0,8,0
+;b 0,1,0,0,0,5,3,9,0
+;b 5,0,2,0,0,0,0,0,8
+;b 9,0,0,1,3,0,0,5,0
+;b 8,0,0,0,4,0,5,0,0
+;b 0,0,3,5,6,8,7,0,0
+;b 0,0,1,0,2,0,0,0,9
+;b 0,8,0,0,5,7,0,0,4
+;b 1,0,0,0,0,0,2,0,3
+;b 0,7,9,3,0,0,0,8,0
+db 0,8,5,0,0,0,0,0,0
+db 0,0,4,0,7,0,0,0,9
+db 0,0,0,0,0,0,0,0,0
+db 3,0,0,0,0,0,0,2,0
+db 7,0,0,0,4,0,0,0,0
+db 0,0,0,1,0,0,0,8,0
+db 0,0,0,2,0,0,0,0,0
+db 9,0,3,0,0,0,0,0,7
+db 0,0,0,8,0,5,0,0,0
 ALIGN 2
 xygmaps:
 xmap dw 9 dup(?)
 ymap dw 9 dup(?)
 gmap dw 9 dup(?)
+xygmaps_end:
 cell_info_count dw ?
-cell_info:
-end start
+cell_info db 81*(SIZE CellInfo) dup(?)
+stack_area dw 81 dup(?)
+end start 
 
